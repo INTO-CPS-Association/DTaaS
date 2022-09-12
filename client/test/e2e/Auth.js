d956fe4dc662ea9bf11c58468ee36cd2fb960cdf
Prasad Talasila <prasadtalasila@users.noreply.github.com>
2022-09-12 11:04:24 +0200

    Corrects github actions config and improves tests (#4)
    * Corrects github actions config and improves tests
    
      - Corrects the github actions config and splits it into
        client and server workflows
      - Generalizes the playwright integration tests
      - Moves the URL of client react application to configuration
        so that playwright tests can work with different URLs.
      - Removes the redundant jshint from syntax checking.
    
    * Adds yarn caching to github workflows

--- client/test/e2e/Auth.js
+++ client/test/e2e/Auth.js
@@ -1,10 +1,12 @@
 // src: https://playwright.dev/docs/writing-tests
 
 import { test, expect } from '@playwright/test';
+// import config from '../../playwright.config';
 
 test.describe('Tests on Authentication Flow', () => {
   test('Homepage has correct title and signin link', async ({ page }) => {
-    await page.goto('http://localhost:4000/');
+  //    await page.goto(config.baseURL);
+    await page.goto('/');
 
     await expect(page).toHaveTitle('The Digital Twin as a Service');
 
@@ -14,7 +16,7 @@ test.describe('Tests on Authentication Flow', () => {
   });
 
   test('Fill the signin fields', async ({ page }) => {
-    await page.goto('http://localhost:4000/');
+    await page.goto('/');
 
     await page.locator('input[name="password"]')
       .fill('dummy password');
@@ -28,14 +30,14 @@ test.describe('Tests on Authentication Flow', () => {
   });
 
   test('Account Button Contents and Links', async ({ page }) => {
-    await page.goto('http://localhost:4000/dashboard');
+    await page.goto('/dashboard');
 
     await page.locator('[aria-label="Open settings"]').click();
     await page.locator('text=Account').click();
-    await expect(page).toHaveURL('http://localhost:4000/account');
+    await expect(page).toHaveURL('/account');
 
     await page.locator('[aria-label="Open settings"]').click();
     await page.locator('text=Logout').click();
-    await expect(page).toHaveURL('http://localhost:4000/');
+    await expect(page).toHaveURL('/');
   });
 });
--- client/test/e2e/Menu.js
+++ client/test/e2e/Menu.js
@@ -4,90 +4,90 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Header Contents and Navigation Links', () => {
   test('Navigation Links on the dashboard page', async ({ page }) => {
-    await page.goto('http://localhost:4000/dashboard');
+    await page.goto('/dashboard');
     await page.locator('div[role="button"]:has-text("Dashboard")').click();
-    await expect(page).toHaveURL('http://localhost:4000/dashboard');
+    await expect(page).toHaveURL('/dashboard');
 
-    await page.goto('http://localhost:4000/dashboard');
+    await page.goto('/dashboard');
     await page.locator('div[role="button"]:has-text("Library")').click();
-    await expect(page).toHaveURL('http://localhost:4000/library');
+    await expect(page).toHaveURL('/library');
 
-    await page.goto('http://localhost:4000/dashboard');
+    await page.goto('/dashboard');
     await page.locator('div[role="button"]:has-text("Digital Twins")').click();
-    await expect(page).toHaveURL('http://localhost:4000/digitaltwins');
+    await expect(page).toHaveURL('/digitaltwins');
 
-    await page.goto('http://localhost:4000/dashboard');
+    await page.goto('/dashboard');
     await page.locator('div[role="button"]:has-text("Scenario Analysis")').click();
-    await expect(page).toHaveURL('http://localhost:4000/sanalysis');
+    await expect(page).toHaveURL('/sanalysis');
 
-    await page.goto('http://localhost:4000/dashboard');
+    await page.goto('/dashboard');
     await page.locator('div[role="button"]:has-text("History")').click();
-    await expect(page).toHaveURL('http://localhost:4000/history');
+    await expect(page).toHaveURL('/history');
   });
 
   test('Navigation Links on the Library page', async ({ page }) => {
-    await page.goto('http://localhost:4000/library');
+    await page.goto('/library');
     await page.locator('div[role="button"]:has-text("Dashboard")').click();
-    await expect(page).toHaveURL('http://localhost:4000/dashboard');
+    await expect(page).toHaveURL('/dashboard');
 
-    await page.goto('http://localhost:4000/library');
+    await page.goto('/library');
     await page.locator('div[role="button"]:has-text("Library")').click();
-    await expect(page).toHaveURL('http://localhost:4000/library');
+    await expect(page).toHaveURL('/library');
 
-    await page.goto('http://localhost:4000/library');
+    await page.goto('/library');
     await page.locator('div[role="button"]:has-text("Digital Twins")').click();
-    await expect(page).toHaveURL('http://localhost:4000/digitaltwins');
+    await expect(page).toHaveURL('/digitaltwins');
 
-    await page.goto('http://localhost:4000/library');
+    await page.goto('/library');
     await page.locator('div[role="button"]:has-text("Scenario Analysis")').click();
-    await expect(page).toHaveURL('http://localhost:4000/sanalysis');
+    await expect(page).toHaveURL('/sanalysis');
 
-    await page.goto('http://localhost:4000/library');
+    await page.goto('/library');
     await page.locator('div[role="button"]:has-text("History")').click();
-    await expect(page).toHaveURL('http://localhost:4000/history');
+    await expect(page).toHaveURL('/history');
   });
 
   test('Navigation Links on the Digital Twins page', async ({ page }) => {
-    await page.goto('http://localhost:4000/digitaltwins');
+    await page.goto('/digitaltwins');
     await page.locator('div[role="button"]:has-text("Dashboard")').click();
-    await expect(page).toHaveURL('http://localhost:4000/dashboard');
+    await expect(page).toHaveURL('/dashboard');
 
-    await page.goto('http://localhost:4000/digitaltwins');
+    await page.goto('/digitaltwins');
     await page.locator('div[role="button"]:has-text("Library")').click();
-    await expect(page).toHaveURL('http://localhost:4000/library');
+    await expect(page).toHaveURL('/library');
 
-    await page.goto('http://localhost:4000/digitaltwins');
+    await page.goto('/digitaltwins');
     await page.locator('div[role="button"]:has-text("Digital Twins")').click();
-    await expect(page).toHaveURL('http://localhost:4000/digitaltwins');
+    await expect(page).toHaveURL('/digitaltwins');
 
-    await page.goto('http://localhost:4000/digitaltwins');
+    await page.goto('/digitaltwins');
     await page.locator('div[role="button"]:has-text("Scenario Analysis")').click();
-    await expect(page).toHaveURL('http://localhost:4000/sanalysis');
+    await expect(page).toHaveURL('/sanalysis');
 
-    await page.goto('http://localhost:4000/digitaltwins');
+    await page.goto('/digitaltwins');
     await page.locator('div[role="button"]:has-text("History")').click();
-    await expect(page).toHaveURL('http://localhost:4000/history');
+    await expect(page).toHaveURL('/history');
   });
 
   test('Navigation Links on the Scenario Analysis page', async ({ page }) => {
-    await page.goto('http://localhost:4000/sanalysis');
+    await page.goto('/sanalysis');
     await page.locator('div[role="button"]:has-text("Dashboard")').click();
-    await expect(page).toHaveURL('http://localhost:4000/dashboard');
+    await expect(page).toHaveURL('/dashboard');
 
-    await page.goto('http://localhost:4000/sanalysis');
+    await page.goto('/sanalysis');
     await page.locator('div[role="button"]:has-text("Library")').click();
-    await expect(page).toHaveURL('http://localhost:4000/library');
+    await expect(page).toHaveURL('/library');
 
-    await page.goto('http://localhost:4000/sanalysis');
+    await page.goto('/sanalysis');
     await page.locator('div[role="button"]:has-text("Digital Twins")').click();
-    await expect(page).toHaveURL('http://localhost:4000/digitaltwins');
+    await expect(page).toHaveURL('/digitaltwins');
 
-    await page.goto('http://localhost:4000/sanalysis');
+    await page.goto('/sanalysis');
     await page.locator('div[role="button"]:has-text("Scenario Analysis")').click();
-    await expect(page).toHaveURL('http://localhost:4000/sanalysis');
+    await expect(page).toHaveURL('/sanalysis');
 
-    await page.goto('http://localhost:4000/sanalysis');
+    await page.goto('/sanalysis');
     await page.locator('div[role="button"]:has-text("History")').click();
-    await expect(page).toHaveURL('http://localhost:4000/history');
+    await expect(page).toHaveURL('/history');
   });
 });
