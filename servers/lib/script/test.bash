a8dbff94ac5aba6b937afcf2fa4ab5655945ba18
ravvnen <phillip.boe.jensen@gmail.com>
2023-02-22 15:55:47 +0100

    creating of service readdir
    This is the service that returns content of the queried directory

--- servers/lib/script/test.bash
+++
@@ -1,3 +0,0 @@
-echo "testing in progress"
-export PATH="$(yarn bin):$PATH"
-jest .
--- servers/lib/src/index.ts
+++
@@ -1,6 +0,0 @@
-function sum(a: number, b: number) {
-  return a + b;
-}
-
-console.log('hello world');
-export default sum;
