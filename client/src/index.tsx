8bf680256e1985bd514986b855adf80f4ebba491
Omar <omarg@live.dk>
2023-02-27 16:47:24 +0100

    Fixed Layout by updating to new Router system

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,8 @@
 import '@fontsource/roboto';
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from 'page/Layout';
+import ReactDOM from 'react-dom/client';
 import Dashboard from './route/dashboard/Dashboard';
 import Library from './route/library/Library';
 import DigitalTwins from './route/digitaltwins/DigitalTwins';
@@ -11,23 +11,42 @@ import DTHistory from './route/history/History';
 import SignIn from './route/auth/Signin';
 import Account from './route/auth/Account';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <SignIn />,
+  },
+  {
+    path: 'dashboard',
+    element: <Layout><Dashboard /></Layout>,
+  },
+  {
+    path: 'library',
+    element: <Layout><Library /></Layout>,
+  },
+  {
+    path: 'digitaltwins',
+    element: <Layout><DigitalTwins /></Layout>,
+  },
+  {
+    path: 'sanalysis',
+    element: <Layout><ScenarioAnalysis /></Layout>,
+  },
+  {
+    path: 'history',
+    element: <Layout><DTHistory /></Layout>,
+  },
+  {
+    path: 'account',
+    element: <Layout><Account /></Layout>,
+  },
+]);
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
-  <>
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="library" element={<Library />} />
-          <Route path="digitaltwins" element={<DigitalTwins />} />
-          <Route path="sanalysis" element={<ScenarioAnalysis />} />
-          <Route path="history" element={<DTHistory />} />
-          <Route path="account" element={<Account />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  </>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
