2858c54a9bd9206a5e75c1c72e5e38f38ff7ecb9
Omar <omarg@live.dk>
2023-03-01 14:54:32 +0100

    Added Iframe to all lib pages

--- client/src/route/library/Components.tsx
+++ client/src/route/library/Components.tsx
@@ -3,37 +3,86 @@ src: https://mui.com/material-ui/react-tabs/
 */
 import * as React from 'react';
 import TabComponent, { TabData } from 'components/tab/TabComponent';
+import Iframe from 'components/Iframe';
+
+const jupyterURL = process.env.REACT_APP_URL_LIB;
 
 const tabs: TabData[] = [
   {
     index: 0 as TabData['index'],
     label: 'Functions',
+    fullsize: true,
     body: (
       <>
         A selection of functions useful for composition of digital twins. The
         functions for data processing and analysis scripts can be placed here.
+        <Iframe
+          title="JupyterLight-Demo"
+          url={jupyterURL || 'google.com'}
+          fullsize={true}
+        />
       </>
     ),
   },
   {
     index: 1 as TabData['index'],
     label: 'Models',
-    body: <>Digital twin models.</>,
+    fullsize: true,
+    body: (
+      <>
+        Digital twin models.
+        <Iframe
+          title="JupyterLight-Demo"
+          url={jupyterURL || 'google.com'}
+          fullsize={true}
+        />
+      </>
+    ),
   },
   {
     index: 2 as TabData['index'],
     label: 'Tools',
-    body: <>Digital twin execution software.</>,
+    fullsize: true,
+    body: (
+      <>
+        Digital twin execution software.
+        <Iframe
+          title="JupyterLight-Demo"
+          url={jupyterURL || 'google.com'}
+          fullsize={true}
+        />
+      </>
+    ),
   },
   {
     index: 3 as TabData['index'],
     label: 'Data',
-    body: <>Data sources for execution of digital twins.</>,
+    fullsize: true,
+    body: (
+      <>
+        Data sources for execution of digital twins.
+        <Iframe
+          title="JupyterLight-Demo"
+          url={jupyterURL || 'google.com'}
+          fullsize={true}
+        />
+      </>
+    ),
   },
   {
     index: 4 as TabData['index'],
     label: 'Digital Twins',
-    body: <>Ready to use digital twins.</>,
+    fullsize: true,
+    body: (
+      <>
+        Ready to use digital twins.
+        <Iframe
+          title="JupyterLight-Demo"
+          url={jupyterURL || 'google.com'}
+          fullsize={true}
+        />
+      </>
+    ),
   },
 ];
 
