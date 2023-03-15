218017660c066e0c1483b0998cbb9093e70f4395
Omar <omarg@live.dk>
2023-03-15 16:04:08 +0100

    Removed todo from test. Supressed output for E2E tests.

--- client/script/test.bash
+++ client/script/test.bash
@@ -4,9 +4,8 @@ export PATH="$(yarn bin):$PATH"
 jest .
 
 # { { yarn start 2>&3 || [ $? -eq 137 ]; } 3>&2 2>/dev/null & } <-- Does not work. Trying to only suppress error 137.
-yarn start 2>/dev/null & # Start server in background. Suppress stderr.
+yarn start >/dev/null & # Start server in background. Suppress stderr.
 printf "\n\n################ Running Playwright ################"
 playwright test
 printf "Closing server on port 4000..."
 npx kill-port 4000
-# TODO: Port should be dynamic. Perhaps saving it in env variable? If applicable for windows.
