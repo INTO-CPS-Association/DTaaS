b4542981cb4a003afb8762f565182e65abf148ce
Asger <71492620+SyntaxXeror@users.noreply.github.com>
2023-03-24 10:55:45 +0100

    Fix codeclimate in iframe test (#20)
    * fix codeclimate in iframe test
    
    * fixed another codeclimate issue

--- client/test/Iframe.test.tsx
+++ client/test/Iframe.test.tsx
@@ -1,38 +1,33 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Iframe from '../src/components/Iframe';
 
 describe('Iframe', () => {
+  var iframe: HTMLIFrameElement;
+
+  beforeEach(() => {
+    render(<Iframe url="https://example.com/" title="Example" />);
+    iframe = screen.getByTitle('Example') as HTMLIFrameElement;
+  });
+
   it('renders an iframe element with the correct src and title', () => {
-    const { getByTitle } = render(
-      <Iframe url="https://example.com/" title="Example" />
-    );
-    const iframe = getByTitle('Example') as HTMLIFrameElement;
     expect(iframe.src).toBe('https://example.com/');
   });
 
   it('sets the iframe width to 100%', () => {
-    const { getByTitle } = render(
-      <Iframe url="https://example.com/" title="Example" />
-    );
-    const iframe = getByTitle('Example') as HTMLIFrameElement;
     expect(iframe.width).toBe('100%');
   });
 
   it('sets the iframe height to 100% if fullsize prop is not provided', () => {
-    const { getByTitle } = render(
-      <Iframe url="https://example.com/" title="Example" />
-    );
-    const iframe = getByTitle('Example') as HTMLIFrameElement;
     expect(iframe.style.height).toBe('100%');
     expect(iframe.style.flexGrow).toBe('');
   });
+});
 
+describe('Iframe fullsize', () => {
   it('sets the iframe height to auto if fullsize prop is provided', () => {
-    const { getByTitle } = render(
-      <Iframe url="https://example.com/" title="Example" fullsize />
-    );
-    const iframe = getByTitle('Example') as HTMLIFrameElement;
+    render(<Iframe url="https://example.com/" title="Example" fullsize />);
+    const iframe = screen.getByTitle('Example') as HTMLIFrameElement;
     expect(iframe.style.flexGrow).toBe('1');
     expect(iframe.style.height).toBe('');
   });
