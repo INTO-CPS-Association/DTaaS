5ac4f3554fa26686b2cf45b9fc0b8ddb02f2a454
Asger <71492620+SyntaxXeror@users.noreply.github.com>
2023-03-24 12:27:57 +0100

    DUGK-31 DigitalTwin unit test (#21)

--- client/test/DigitalTwins.test.tsx
+++ client/test/DigitalTwins.test.tsx
@@ -1,10 +1,19 @@
-import { render } from '@testing-library/react';
 import * as React from 'react';
+import { render, screen } from '@testing-library/react';
 import DigitalTwins from '../src/route/digitaltwins/DigitalTwins';
 
+jest.mock('route/digitaltwins/Workflows', () => ({
+  default: () => <div>workflows-mock</div>,
+}));
+
 describe('Digital Twins', () => {
-  it('Reder OK', () => {
+  it('reder DigitalTwin', () => {
     render(<DigitalTwins />);
     expect(true);
+  });
+
+  it('renders Dashboard woth components', () => {
+    render(<DigitalTwins />);
+    expect(screen.queryByText('workflows-mock')).toBeInTheDocument();
   });
 });
