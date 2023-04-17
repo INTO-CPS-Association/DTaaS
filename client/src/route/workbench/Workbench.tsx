d7b3ebf145dc63a18afa680ea718da9827bea2d2
Asger <71492620+SyntaxXeror@users.noreply.github.com>
2023-04-17 18:22:13 +0200

    Small refactor of Workbench (#46)

--- client/src/route/workbench/Workbench.tsx
+++ client/src/route/workbench/Workbench.tsx
@@ -3,12 +3,13 @@ import Iframe from 'components/Iframe';
 import Layout from 'page/Layout';
 import * as React from 'react';
 
+const UrlWorkbench = window.env.REACT_APP_URL_WORKBENCH;
+
 function WorkBenchContent() {
   /*
   Will become configurable in the next iteration
   const UrlWorkbench = new URL('user/2/tree?', window.location.href);
   */
-  const UrlWorkbench = window.env.REACT_APP_URL_WORKBENCH;
 
   return (
     <Layout>
