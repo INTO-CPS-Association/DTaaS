b6f3063cc4fe90719bdf77a63c289c4fc352df86
Asger <71492620+SyntaxXeror@users.noreply.github.com>
2023-04-17 18:13:26 +0200

    DUGK-68 Removed 'Components.tsx' and renamed '...Data.ts' (#47)

--- client/src/route/library/Library.tsx
+++ client/src/route/library/Library.tsx
@@ -1,12 +1,30 @@
 import * as React from 'react';
-
 import Layout from 'page/Layout';
-import LibComponents from './Components';
+import TabComponent from 'components/tab/TabComponent';
+import Iframe from 'components/Iframe';
+import { TabData } from 'components/tab/subcomponents/TabRender';
+import tabs from './LibraryTabData';
+
+const jupyterURL = window.env.REACT_APP_URL_LIB;
+
+const tabsData: TabData[] = tabs.map((tab) => ({
+  label: tab.label,
+  body: (
+    <>
+      {tab.body}
+      <Iframe
+        title={`JupyterLight-Demo-${tab.label}`}
+        url={`${jupyterURL}`}
+        fullsize
+      />
+    </>
+  ),
+}));
 
 function LibraryContent() {
   return (
     <Layout>
-      <LibComponents />
+      <TabComponent tabs={tabsData} />
     </Layout>
   );
 }
