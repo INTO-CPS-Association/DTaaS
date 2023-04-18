23828197bb3a58dd44673ea8650b2d0eec38c492
Omar <omarg@live.dk>
2023-04-19 01:14:45 +0200

    Tempoary fix on CI pipeline with unittest. If coverage is not met, the whole pipeline breaks instead of continuing. This temp fix will run the tests twice. Collect coverage during second.

--- client/script/test.bash
+++ client/script/test.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
-printf "\n\n################ Testing in progress ################"
+printf "\n\n################ Testing in progress ################\n"
 printf "\n################ Running Jest ################\n"
 PATH="$(yarn bin):$PATH" # Add yarn bin to path
 export PATH
-jest .
+jest . --collectCoverage=false
 
 JEST_EXIT_CODE=$?
 if [ $JEST_EXIT_CODE -ne 0 ]; then
@@ -13,7 +13,7 @@ fi
 
 # { { yarn start 2>&3 || [ $? -eq 137 ]; } 3>&2 2>/dev/null & } <-- Does not work. Trying to only suppress error 137.
 yarn start >/dev/null & # Start server in background. Suppress stderr.
-printf "\n\n################ Running Playwright ################"
+printf "\n\n################ Running Playwright ################\n"
 playwright test
 PLAYWRGHT_EXIT_CODE=$?
 
@@ -21,6 +21,11 @@ printf "Closing server on port 4000..."
 npx kill-port 4000
 
 if [ $PLAYWRGHT_EXIT_CODE -ne 0 ]; then
-    printf "\n\n################ Playwright tests failed ################"
-    exit 1
+    printf "\n\n################ Playwright tests failed ################\n"
+    exit $PLAYWRGHT_EXIT_CODE
 fi
+
+printf "\n\n################ Testing finished ################\n"
+
+printf "\n\n################ Collecting coverage unittest ################\n"
+jest . --collectCoverage=true
