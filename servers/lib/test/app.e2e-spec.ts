02f04d14c0cd684496e38a4d6d742c90b29a7611
ravvnen <phillip.boe.jensen@gmail.com>
2023-04-27 13:43:56 +0200

    integration test for traefik and lib microservice

---
+++ servers/lib/test/app.e2e-spec.ts
@@ -0,0 +1,39 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { AppModule } from "../src/app.module";
+
+describe("End to End test for the application", () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should return the filename corresponding to the directory given in the query", async () => {
+    const path = "user1";
+    const query = `{
+      getFiles(path: "${path}")
+  }`;
+
+    const expectedResponse = {
+      data: {
+        getFiles: ["data", "digital twins", "functions", "models", "tools"],
+      },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post("/lib")
+      .send({ query });
+    expect(response.body).toEqual(expectedResponse);
+  });
+});
