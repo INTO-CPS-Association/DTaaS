54d79ab92de951641ceefdc856bf9101e08da8e1
Omar <omarg@live.dk>
2023-04-30 12:50:59 +0200

    Fixed env files

--- client/config/dev.js
+++ client/config/dev.js
@@ -4,7 +4,7 @@ window.env = {
   REACT_APP_URL_DT: 'https://jupyterlite.github.io/demo/repl/index.html',
   REACT_APP_URL_WORKBENCH: 'https://ece.au.dk/',
   REACT_APP_WORKBENCHLINK_TERMINAL: '/terminals/main',
-  REACT_APP_WORKBENCHLINK_VNCDESKTOP: '/tools/vnc/?password=',
+  REACT_APP_WORKBENCHLINK_VNCDESKTOP: '/tools/vnc/?password=vncpassword',
   REACT_APP_WORKBENCHLINK_VSCODE: '/tools/vscode/',
   REACT_APP_WORKBENCHLINK_JUPYTERLAB: '/lab',
   REACT_APP_WORKBENCHLINK_JUPYTERNOTEBOOK: '',
