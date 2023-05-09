4b108ea324b070d138ed032395ea9792a9636c6b
ravvnen <phillip.boe.jensen@gmail.com>
2023-05-09 10:26:06 +0200

    automate test w traefik and lib

---
+++ servers/lib/test/stoptraefik.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ../../servers/config/gateway
+
+echo "Stopping the Traefik gateway"
+docker stop traefik-gateway
+
+echo "Removing the Traefik gateway container"
+docker rm /traefik-gateway
