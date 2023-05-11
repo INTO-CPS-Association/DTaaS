cea10a413336c9e6e03083f7d37b5d5fe468ece6
ravvnen <phillip.boe.jensen@gmail.com>
2023-05-11 14:38:14 +0200

    move traefik test, structure tests

---
+++ servers/lib/test/files.resolver.unit.spec.ts
@@ -0,0 +1,39 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { FilesService } from "../../src/files/files.service";
+import { FilesResolver } from "../../src/files/files.resolver";
+import { files, path } from "../testUtil";
+
+describe("Unit tests for FilesResolver", () => {
+  let filesResolver: FilesResolver;
+
+  const mockFilesService = {
+    Wrapper: jest.fn(() => files),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [FilesResolver, FilesService],
+    })
+      .overrideProvider(FilesService)
+      .useValue(mockFilesService)
+      .compile();
+
+    filesResolver = module.get<FilesResolver>(FilesResolver);
+  });
+
+  it("should be defined", () => {
+    expect(filesResolver).toBeDefined();
+  });
+
+  describe("getFiles", () => {
+    it("should be defined", () => {
+      expect(filesResolver.getFiles).toBeDefined();
+    });
+
+    it("should return the filenames in the given directory", async () => {
+      const result = await filesResolver.getFiles(path);
+
+      expect(result).toEqual(files);
+    });
+  });
+});
