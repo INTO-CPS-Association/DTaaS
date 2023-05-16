9d0c16789cb35353baae77767b03c0f9aa007f4a
Phillip Ravn Boe Jensen <35298645+ravvnen@users.noreply.github.com>
2023-05-16 08:58:13 +0200

    Adds traefik support for lib microservice (#54)
      - Updates and refactors lib microservice code to work
         traefik gateway
      - Updates lib microservice documentation

--- servers/lib/script/test.bash
+++ servers/lib/script/test.bash
@@ -4,6 +4,7 @@ new_path="$(yarn bin):$PATH"
 export PATH="$new_path"
 test_path=$(pwd)/test/data/test_assets
 export TEST_PATH="$test_path"
+
 # Get the first argument passed to the script
 mode=$1
 
