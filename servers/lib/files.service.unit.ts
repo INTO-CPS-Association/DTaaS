acf25c8b4ba552b3f7d0e9c1d81564b8a5d12083
ravvnen <phillip.boe.jensen@gmail.com>
2023-05-21 11:39:11 +0200

    fix unit test - invalidpath

--- servers/lib/files.service.unit.ts
+++ servers/lib/files.service.unit.ts
@@ -96,7 +96,8 @@ describe("Unit tests for FilesService", () => {
         .spyOn(filesService, "getGitlabFiles")
         .mockResolvedValue(testDirectory);
 
-      const result = await filesService.Wrapper(pathToTestDirectory);
+      const invalidPath = "invalid_path";
+      const result = await filesService.Wrapper(invalidPath);
 
       expect(result).toEqual(testDirectory);
 
