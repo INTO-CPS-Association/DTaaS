8889db747a6b5047537c3da3c905e27071686810
ravvnen <phillip.boe.jensen@gmail.com>
2023-05-30 23:08:21 +0200

    remove unused import

--- servers/lib/test/e2e/app.e2e.spec.ts
+++ servers/lib/test/e2e/app.e2e.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
 import { AppModule } from "../../src/app.module";
-import { e2eDirectory, pathToTestDirectory, testDirectory } from "../testUtil";
+import { e2eDirectory, pathToTestDirectory } from "../testUtil";
 
 describe("End to End test for the application", () => {
   let app: INestApplication;
