b17b47f8380d8e8db48054a7157d316cefe2dfa3
ravvnen <phillip.boe.jensen@gmail.com>
2023-05-30 22:47:11 +0200

    change to lowercase

---
+++ servers/lib/test/e2e/app.traefik.e2e.spec.ts
@@ -0,0 +1,112 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import * as request from "supertest";
+
+describe("End to End test for the application", () => {
+  it("should return the filename corresponding to the directory given in the query through the Traefik gateway", async () => {
+    const query = `query {
+      listDirectory(path:"user2")
+      {
+        repository{
+          tree{
+            trees{
+              edges{
+                node{
+                  name
+                  
+                }
+              }
+            }
+          }
+        }
+      }
+    }`;
+
+    const expectedResponse = {
+      data: {
+        listDirectory: {
+          repository: {
+            tree: {
+              trees: {
+                edges: [
+                  {
+                    node: {
+                      name: "data",
+                    },
+                  },
+                  {
+                    node: {
+                      name: "digital twins",
+                    },
+                  },
+                  {
+                    node: {
+                      name: "functions",
+                    },
+                  },
+                  {
+                    node: {
+                      name: "models",
+                    },
+                  },
+                  {
+                    node: {
+                      name: "tools",
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const response = await request("http://localhost:4001")
+      .post("/lib")
+      .send({ query });
+
+    response;
+    expect(response.body).toEqual(expectedResponse);
+  }, 10000);
+
+  it("should return the content of a file given in the query through the Traefik gateway", async () => {
+    const query = `query {
+      readFile(path:"user2/Tools/README.md") {
+        repository {
+          blobs {
+            nodes {
+              name
+              rawBlob
+              rawTextBlob
+            }
+          }
+        }
+      }
+    }`;
+
+    const expectedResponse = {
+      data: {
+        readFile: {
+          repository: {
+            blobs: {
+              nodes: [
+                {
+                  name: "README.md",
+                  rawBlob: "content123",
+                  rawTextBlob: "content123",
+                },
+              ],
+            },
+          },
+        },
+      },
+    };
+
+    const response = await request("http://localhost:4001")
+      .post("/lib")
+      .send({ query });
+
+    response;
+    expect(response.body).toEqual(expectedResponse);
+  }, 10000);
+});
