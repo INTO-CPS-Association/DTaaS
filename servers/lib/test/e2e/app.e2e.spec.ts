083f3c247cf6a45dced51f2f137eea596720b7ea
ravvnen <phillip.boe.jensen@gmail.com>
2023-06-04 12:29:24 +0200

    smooth e2e test swap

--- servers/lib/test/e2e/app.e2e.spec.ts
+++ servers/lib/test/e2e/app.e2e.spec.ts
@@ -10,42 +10,68 @@ import {
   expectedListDirectoryResponse,
 } from "../testUtil";
 
-describe("End to End test for the application", () => {
-  let app: INestApplication;
+if (process.env.TEST_VERSION === "local") {
+  describe("End to End test for the application", () => {
+    let app: INestApplication;
 
-  beforeAll(async () => {
-    //execSync("test/starttraefik.bash");
+    beforeAll(async () => {
+      //execSync("test/starttraefik.bash");
 
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-    app = moduleFixture.createNestApplication();
-    await app.init(); // Initialize the application
-    await app.listen(process.env.PORT);
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
+      app = moduleFixture.createNestApplication();
+      await app.init(); // Initialize the application
+      await app.listen(process.env.PORT);
 
-    await new Promise((resolve) => setTimeout(resolve, 4000));
-  }, 10000);
+      await new Promise((resolve) => setTimeout(resolve, 4000));
+    }, 10000);
 
-  afterAll(async () => {
-    //execSync("test/stoptraefik.bash");
-    await app.close();
-  }, 10000);
+    afterAll(async () => {
+      //execSync("test/stoptraefik.bash");
+      await app.close();
+    }, 10000);
 
-  it("should return the filename corresponding to the directory given in the query through the Traefik gateway", async () => {
-    const query = e2elistDirectory;
+    it("should return the filename corresponding to the directory given in the query through the Traefik gateway", async () => {
+      const query = e2elistDirectory;
 
-    const response = await request("http://localhost:4001")
-      .post(process.env.APOLLO_PATH)
-      .send({ query });
-    expect(response.body).toEqual(expectedListDirectoryResponse);
-  }, 10000);
+      const response = await request("http://localhost:4001")
+        .post(process.env.APOLLO_PATH)
+        .send({ query });
+      expect(response.body).toEqual(expectedListDirectoryResponse);
+    }, 10000);
 
-  it("should return the content of a file given in the query through the Traefik gateway", async () => {
-    const query = e2eReadFile;
+    it("should return the content of a file given in the query through the Traefik gateway", async () => {
+      const query = e2eReadFile;
 
-    const response = await request("http://localhost:4001")
-      .post(process.env.APOLLO_PATH)
-      .send({ query });
-    expect(response.body).toEqual(expectedFileContentResponse);
-  }, 10000);
-});
+      const response = await request("http://localhost:4001")
+        .post(process.env.APOLLO_PATH)
+        .send({ query });
+      expect(response.body).toEqual(expectedFileContentResponse);
+    }, 10000);
+  });
+} else if (process.env.TEST_VERSION === "traefik") {
+  describe("End to End test for the application", () => {
+    it("should return the filename corresponding to the directory given in the query through the Traefik gateway", async () => {
+      const query = e2elistDirectory;
+
+      const response = await request("http://localhost")
+        .post("/lib")
+        .send({ query });
+
+      response;
+      expect(response.body).toEqual(expectedListDirectoryResponse);
+    }, 10000);
+
+    it("should return the content of a file given in the query through the Traefik gateway", async () => {
+      const query = e2eReadFile;
+
+      const response = await request("http://localhost")
+        .post("/lib")
+        .send({ query });
+
+      response;
+      expect(response.body).toEqual(expectedFileContentResponse);
+    }, 10000);
+  });
+}
