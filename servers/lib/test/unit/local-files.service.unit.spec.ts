2660fae068c512dab77470f4414248283665b777
ravvnen <phillip.boe.jensen@gmail.com>
2023-06-04 13:00:47 +0200

    fix local service test

--- servers/lib/test/unit/local-files.service.unit.spec.ts
+++ servers/lib/test/unit/local-files.service.unit.spec.ts
@@ -1,42 +1,99 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { ConfigService } from "@nestjs/config";
-import { GitlabFilesService } from "../../src/files/services/gitlab-files.service";
-import { pathToTestFileContent, testFileContent } from "../testUtil";
-import { MockConfigService, testDirectory } from "../testUtil";
-import axios from "axios";
+import { LocalFilesService } from "../../src/files/services/local-files.service";
+import * as fs from "fs";
+import { join } from "path";
+import { pathToTestDirectory, testFileArray } from "../testUtil";
 
-describe("GitlabFilesService", () => {
-  let filesService: GitlabFilesService;
-  const mockConfigService = new MockConfigService();
-  jest.mock("axios");
+jest.mock("fs", () => ({
+  promises: {
+    readdir: jest.fn(),
+    lstat: jest.fn(),
+    readFile: jest.fn(),
+  },
+}));
+
+describe("LocalFilesService", () => {
+  let service: LocalFilesService;
+  let configService: ConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        GitlabFilesService,
-        { provide: ConfigService, useValue: mockConfigService },
+        LocalFilesService,
+        {
+          provide: ConfigService,
+          useValue: { get: jest.fn().mockReturnValue("mocked_path") },
+        },
       ],
-    })
-      .overrideProvider(ConfigService)
-      .useValue(mockConfigService)
-      .compile();
+    }).compile();
 
-    filesService = module.get<GitlabFilesService>(GitlabFilesService);
+    service = module.get<LocalFilesService>(LocalFilesService);
+    configService = module.get<ConfigService>(ConfigService);
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should be defined", () => {
+    expect(service).toBeDefined();
   });
 
   it("should list directory", async () => {
-    jest.spyOn(axios, "post").mockResolvedValue({ data: testDirectory });
+    const fullPath = join(configService.get("LOCAL_PATH"), pathToTestDirectory);
 
-    const result = await filesService.listDirectory("user2");
-    expect(result).toEqual(testDirectory);
+    const direntMock = testFileArray.map((file) => ({
+      name: file,
+      isDirectory: () => true,
+    }));
+
+    // Mock Stats value for lstat
+    const statsMock: Partial<fs.Stats> = {
+      isDirectory: () => true, // change this to false when you need a file instead of a directory
+    };
+
+    jest.spyOn(fs.promises, "readdir").mockResolvedValue(testFileArray as any);
+
+    jest
+      .spyOn(fs.promises, "lstat")
+      .mockImplementation((pathToTestDirectory) => {
+        if (typeof pathToTestDirectory === "string") {
+          return Promise.resolve(statsMock as fs.Stats);
+        }
+        throw new Error(`Invalid argument: ${pathToTestDirectory}`);
+      });
+    const result = await service.listDirectory(pathToTestDirectory);
+    expect(result).toEqual({
+      repository: {
+        tree: {
+          trees: {
+            edges: testFileArray.map((file) => ({
+              node: { name: file, type: "tree" },
+            })),
+          },
+          blobs: { edges: [] },
+        },
+      },
+    });
+    expect(fs.promises.readdir).toHaveBeenCalledWith(fullPath);
+    expect(fs.promises.lstat).toHaveBeenCalledTimes(testFileArray.length);
   });
 
   it("should read file", async () => {
-    jest
-      .spyOn(axios, "post")
-      .mockResolvedValue({ data: { data: testFileContent } });
+    const path = "test_path/test_file";
+    const content = "test content";
+    const name = "test_file";
+    const fullPath = join(configService.get("LOCAL_PATH"), path);
 
-    const result = await filesService.readFile(pathToTestFileContent);
-    expect(result).toEqual(testFileContent);
+    jest.spyOn(fs.promises, "readFile").mockResolvedValue(content);
+
+    const result = await service.readFile(path);
+    expect(result).toEqual({
+      repository: {
+        blobs: { nodes: [{ name, rawBlob: content, rawTextBlob: content }] },
+      },
+    });
+    expect(fs.promises.readFile).toHaveBeenCalledWith(fullPath, "utf8");
   });
 });
