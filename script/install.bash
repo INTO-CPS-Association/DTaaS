175789e8bc14e2966dd6f62ac655c20b54ce0521
ravvnen <phillip.boe.jensen@gmail.com>
2023-06-07 13:39:39 +0200

    final changes

--- script/install.bash
+++ script/install.bash
@@ -59,6 +59,3 @@ sudo -H pip install jupyterlab
 #install docker-compose from https://docs.docker.com/compose/install/other/
 curl -SL "https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-#install axios
-yarn add axios
