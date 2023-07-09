80498d17eef75ac85f98fb8e3ed544fa25836887
Omarley7 <omarg@live.dk>
2023-07-09 12:50:39 +0200

    Changes needed after Rebase

--- client/config/ci.js
+++ client/config/ci.js
@@ -9,6 +9,13 @@ window.env = {
     REACT_APP_WORKBENCHLINK_VSCODE: '/tools/vscode/',
     REACT_APP_WORKBENCHLINK_JUPYTERLAB: '/lab',
     REACT_APP_WORKBENCHLINK_JUPYTERNOTEBOOK: '',
+    
     REACT_APP_BACKEND_URL_GITLAB: 'https://gitlab.com/api/graphql',
-    REACT_APP_BACKEND_GITLAB_GROUP: 'dtaas1'
+    REACT_APP_BACKEND_GITLAB_GROUP: 'dtaas1',
+
+    REACT_APP_CLIENT_ID: '934b98f03f1b6f743832b2840bf7cccaed93c3bfe579093dd0942a433691ccc0',
+    REACT_APP_AUTH_AUTHORITY: 'https://gitlab.foo.com/',
+    REACT_APP_REDIRECT_URI: 'https://foo.com/Library',
+    REACT_APP_LOGOUT_REDIRECT_URI: 'https://foo.com/',
+    REACT_APP_GITLAB_SCOPES: 'openid profile read_user read_repository api',
 }; 
--- client/test/unitTests/Routes/Library.test.tsx
+++ client/test/unitTests/Routes/Library.test.tsx
@@ -4,6 +4,14 @@ import tabs from 'route/library/LibraryTabData';
 import AuthProvider from '../../../src/route/auth/AuthProvider';
 import { InitRouteTests, itDisplaysContentOfTabs } from '../testUtils';
 
+// Only needed while Authentication is using Redux directly. Once Authentication
+// is using an Access module (e.g. UserAccess), this can be removed and replced with a mock of
+// the Access module.
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: () => jest.fn(),
+}));
+
 describe('Library with no props', () => {
   InitRouteTests(
     <AuthProvider>
