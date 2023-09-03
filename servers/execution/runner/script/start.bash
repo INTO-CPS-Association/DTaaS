572ccfdad091ee342fbfde2247d958df5356056b
prasadtalasila <prasad.talasila@gmail.com>
2023-09-03 14:03:39 +0200

    Adds skeleton code for nestjs framework

--- servers/execution/runner/script/start.bash
+++ servers/execution/runner/script/start.bash
@@ -3,4 +3,5 @@ printf "starting the node application\n\n"
 
 cross-env NODE_OPTIONS=--es-module-specifier-resolution=node \
   NODE_NO_WARNINGS=1 \
-  node dist/src/runner.js
+#  node dist/src/runner.js
+  node dist/src/main.js
