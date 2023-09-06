90703ec90ab31fd2e074be2ce200348012c123e1
nichlaes <nichlaes.soerensen@gmail.com>
2023-09-06 15:58:22 +0200

    fix local build errors

--- servers/execution/runner/script/test.bash
+++ servers/execution/runner/script/test.bash
@@ -4,6 +4,6 @@ printf "testing in progress"
 new_path="$(yarn bin):$PATH"
 export PATH="$new_path"
 
-cross-env NODE_OPTIONS=--experimental-vm-modules \
+npx cross-env NODE_OPTIONS=--experimental-vm-modules \
   NODE_NO_WARNINGS=1 \
   jest --coverage
