2fe03444bc492e509015659359ff3a057ce73fa7
prasadtalasila <prasad.talasila@gmail.com>
2023-09-17 07:17:04 +0200

    Adds API Specification for DT Runner  and updates shell scripts
      - Add OpenAPI specification for DT Runner
      - Updates git-hooks to check against runner as well
      - Adds more yarn test commands to runner

--- servers/execution/runner/script/test.bash
+++ servers/execution/runner/script/test.bash
@@ -2,8 +2,20 @@
 
 printf "testing in progress"
 new_path="$(yarn bin):$PATH"
-export PATH="$new_path"
+PATH="$new_path"
+export PATH
+
+if [ "$1" == "nocoverage" ]; then
+  COV_FLAG="false"
+  WATCH_FLAG=""
+elif [ "$1" == "watchAll" ]; then
+  COV_FLAG="true"
+  WATCH_FLAG="--watchAll"
+else
+  COV_FLAG="true"
+  WATCH_FLAG=""
+fi
 
 npx cross-env NODE_OPTIONS=--experimental-vm-modules \
   NODE_NO_WARNINGS=1 \
-  jest --coverage "$1"
+  jest --coverage="${COV_FLAG}" "$WATCH_FLAG"
