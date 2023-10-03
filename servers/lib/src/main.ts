0120b18e6bafc36ce692c87fe6d4d59361b905f7
Linda Nguyen <92531748+lindot11@users.noreply.github.com>
2023-10-03 15:09:40 +0200

    small typo fix

--- servers/lib/src/main.ts
+++ servers/lib/src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>(("PORT");
+  const port = configService.get<number>("PORT");
   await app.listen(port);
 }
 
