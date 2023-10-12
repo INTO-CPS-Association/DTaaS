e6cee12f04f8b0b58b5dd1970102f99ef6745d29
Linda Nguyen <92531748+lindot11@users.noreply.github.com>
2023-10-12 13:31:31 +0200

    trying again using this to fix lint checks

--- servers/lib/src/libms.command.ts
+++ servers/lib/src/libms.command.ts
@@ -7,17 +7,21 @@ import { parse } from 'dotenv';
 })
 
 export default class LibmsCommand extends CommandRunner {
+    private configFile: string;
+
     async run(_inputs: string[], options: Record<string, string>): Promise<void> 
     {
-        const configFile = this.parseConfigFile(options.config);
-        const config = readFileSync(configFile);
+        if (options.config) {
+            this.parseConfigFile(options.config);
+        }
+        const config = readFileSync(this.configFile);
         parse(config);
     }
     @Option({
         flags: '-c, --config <config-file>',
         description: 'Configuration file',
     })
-    parseConfigFile(configFile: string): string {
-        return configFile;
+    parseConfigFile(configFile: string): void {
+        this.configFile = configFile;
     }
 }
