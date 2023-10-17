8c932fe1555130a243a64497f14a9c67e1fcc265
Mads Kelberg <madskelberg@tutanota.com>
2023-10-17 21:37:04 +0200

    Added yarn.lock file & made some changes to the CLI commands

--- servers/lib/src/bootstrap.ts
+++ servers/lib/src/bootstrap.ts
@@ -5,14 +5,19 @@ import AppModule from "./app.module";
 
 type BootstrapOptions = {
   config?: string
+  runHelp?: CallableFunction
 }
 
 export default async function bootstrap(options?: BootstrapOptions) {
   const configFile = dotenv.config({path: options?.config ?? ".env", override: true})
   if (configFile.error) {
-    // eslint-disable-next-line
+    // eslint-disable-next-line no-console
     console.error(configFile.error);
-    process.exit(1);
+    if (options.runHelp) {
+      options.runHelp()
+    } else {
+      process.exit(1)
+    }
   };
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
