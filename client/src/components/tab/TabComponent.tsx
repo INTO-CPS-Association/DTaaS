6b66102ff3038c846d19fa745950be2e8e587066
s88004 <s88004@bht-berlin.de>
2023-11-13 12:57:50 +0100

    removed space like he wanted

--- client/src/components/tab/TabComponent.tsx
+++ client/src/components/tab/TabComponent.tsx
@@ -1,13 +1,7 @@
 import * as React from 'react';
-
-// import Iframe from 'react-iframe';
-// import { useURLforLIB } from 'util/envUtil';
-
-import { Paper } from '@mui/material';
 import { cleanURL } from 'util/envUtil';
 import TabRender, { TabData } from './subcomponents/TabRender';
 import { Tab, TabList, TabPanel, Tabs } from './subcomponents/TabStyles';
-// import { assetType } from '../../route/library/LibraryTabData';
 
 export function constructURL(assetType: string, scope: string, libURL: string) {
   const formattedTab = assetType.toLowerCase();
@@ -36,56 +30,33 @@ export function constructURL(assetType: string, scope: string, libURL: string) {
  */
 
 export function TabComponent(props: { assetType: TabData[], scope: TabData[][] }) {
-  // const LIBurl = useURLforLIB();
+
   return (
-    <Paper
-      sx={{ p: 2, display: 'flex', flexDirection: 'column', minHeight: '100%' }}
-    >
-      <Tabs>
-        <TabList>
-          {props.assetType.map((tab, index) => (
-            <Tab key={index}>{tab.label}</Tab>
-          ))}
-        </TabList>
-        {props.assetType.map((tab1, index1) => (
-          <TabPanel key={index1}>
-            <TabRender index={index1}>{tab1}</TabRender>
-
-            <Tabs forceRenderTabPanel>
-              <TabList>
-                {props.scope[index1].map((tab, index) => (
-                  <Tab key={index}>{tab.label}</Tab>
-                ))}
-              </TabList>
-              {props.scope[index1].map((tab, index) => (
-                <TabPanel key={index}>
-                  <TabRender index={index}>{tab}</TabRender>
-                </TabPanel>
-              ))}
-            </Tabs>
-
-            {/* <Tabs forceRenderTabPanel>
-              <TabList>
-                {scope.map((subTab, subIndex) => (
-                  <Tab 
-                    key={subIndex}>{subTab.label}
-                  </Tab>
-                ))}{' '}
-              </TabList>
-              {scope.map((subTab, subIndex) => (
-                <TabPanel key={subIndex}>
-                  <TabRender index={index}>{tab} </TabRender>
-                  <Iframe
-                    title={`${tab.label}`}
-                    url={constructURL(tab.label, subTab.label, LIBurl)}
-                  />
-                </TabPanel>
-              ))}
-            </Tabs> */}
-          </TabPanel>
+    <Tabs>
+      <TabList>
+        {props.assetType.map((tab, index) => (
+          <Tab key={index}>{tab.label}</Tab>
         ))}
-      </Tabs>
-    </Paper>
+      </TabList>
+      {props.assetType.map((tab1, index1) => (
+        <TabPanel key={index1}>
+          <TabRender index={index1}>{tab1}</TabRender>
+
+          <Tabs forceRenderTabPanel>
+            <TabList>
+              {props.scope[index1].map((tab, index) => (
+                <Tab key={index}>{tab.label}</Tab>
+              ))}
+            </TabList>
+            {props.scope[index1].map((tab, index) => (
+              <TabPanel key={index}>
+                <TabRender index={index}>{tab}</TabRender>
+              </TabPanel>
+            ))}
+          </Tabs>
+        </TabPanel>
+      ))}
+    </Tabs>
   );
 }
 
