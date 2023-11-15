c52f8e13849df7f34046958c7711dc877d5f1591
prasadtalasila <prasad.talasila@gmail.com>
2023-11-15 21:52:50 +0100

    Adds skeleton code for configuration

--- servers/execution/runner/src/app.module.ts
+++ servers/execution/runner/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import AppController from './app.controller';
 import LifeCycleManager from './lifecycleManager.service';
 import Queue from './queue.service';
+import configuration from './config/configuration';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      load: [configuration],
+    }),
+  ],
   controllers: [AppController],
   providers: [LifeCycleManager, Queue],
 })
