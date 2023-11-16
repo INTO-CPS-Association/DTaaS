c619d8425c0c5e092afa9a5ad325a7cc64be1f64
prasadtalasila <prasad.talasila@gmail.com>
2023-11-16 20:44:42 +0100

    Removes skeleton code for config within runner component

--- servers/execution/runner/src/app.module.ts
+++ servers/execution/runner/src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import AppController from './app.controller';
 import LifeCycleManager from './lifecycleManager.service';
 import Queue from './queue.service';
-import configuration from './config/configuration';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      ignoreEnvFile: true,
-      load: [configuration],
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [LifeCycleManager, Queue],
 })
