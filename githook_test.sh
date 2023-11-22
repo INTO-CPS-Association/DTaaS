2e9758330dc39f10e6a3ccc73ae93229d007a30f
lindot11 <lindaspostboks@gmail.com>
2023-11-22 16:45:33 +0100

    reset after test

--- githook_test.sh
+++ githook_test.sh
@@ -14,25 +14,28 @@
 # git push -q
 
 # 2. test prettier hook fail
-printf "Testing prettier hook fail...\n\n"
+# printf "Testing prettier hook fail...\n\n"
+
+# printf "Creating dummy files...\n\n"
+# echo "const dummy   = 'Hello, client';" > client/dummy.ts
+
+# printf "Staging changes...\n\n"
+# git add .
+# printf "Pre-commit stage...\n\n"
+# git commit -m "test git hooks" -q
+# git reset -q
+
+# 3. test eslint hook fail
+print "Testing eslint hook fail...\n\n"
 
 printf "Creating dummy files...\n\n"
-echo "const dummy   = 'Hello, client';" > client/dummy.ts
+echo "const dummy = 'Hello, runner';" > servers/execution/runner/dummy.js
 
 printf "Staging changes...\n\n"
 git add .
 printf "Pre-commit stage...\n\n"
 git commit -m "test git hooks" -q
 git reset -q
-
-# 3. test eslint hook fail
-# printf "Creating dummy files...\n\n"
-# echo "const dummy = 'Hello, runner';" > servers/execution/runner/dummy.js
-
-# printf "Testing eslint hook fail...\n\n"
-# git add .
-# git commit -m "test eslint hook"
-# git reset
 
 # 4. test markdownlint hook fail
 # printf "Creating dummy files...\n\n"
@@ -44,12 +47,10 @@ git reset -q
 # git reset
 
 # clean up after each test
-printf "\n Removing dummy files...\n\n"
 rm -f client/dummy.ts
 rm -f servers/execution/runner/dummy.js
 rm -f servers/lib/dummy.md
 
-printf "Resetting...\n\n"
 git add .
 git commit -m "reset after test" -q
 git push -q
