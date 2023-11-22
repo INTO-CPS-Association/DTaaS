64415e9f04cc9182179e65cf32f124afb95516ec
lindot11 <lindaspostboks@gmail.com>
2023-11-22 14:19:19 +0100

    test git hooks

--- githook_test.sh
+++ githook_test.sh
@@ -1,34 +1,42 @@
 TOP_DIR="$(pwd)"
 
-# clean
-cd client
-yarn clean
-cd "$TOP_DIR"
+# # setup
+# printf "Setting up...\n\n"
+# cd client
+# yarn clean
+# cd "$TOP_DIR"
 
-cd servers/execution/runner
-yarn clean
-cd "$TOP_DIR"
+# cd servers/execution/runner
+# yarn clean
+# cd "$TOP_DIR"
 
-cd servers/lib
-yarn clean
-cd "$TOP_DIR"
+# cd servers/lib
+# yarn clean
+# cd "$TOP_DIR"
+
+# rm -rf .git/hooks/
+
+# # install
+# printf "Installing...\n\n"
+# pip install pre-commit
+# pre-commit autoupdate
+# pre-commit install
 
 # dummy
+printf "Creating dummy files...\n\n"
 echo "const dummy = 'Hello, client!';" > client/dummy.ts
 echo "const dummy = 'Hello, runner!';" > servers/execution/runner/dummy.ts
 echo "const dummy = 'Hello, lib!';" > servers/lib/dummy.ts
 
-# install
-pip install pre-commit
-pre-commit autoupdate
-pre-commit install
-
 # test
+printf "Testing...\n\n"
 git add .
 git commit -m "test git hooks"
+git add .
 git push
 
 # remove
+printf "Removing dummy files...\n\n"
 rm -f client/dummy.ts
 rm -f servers/execution/runner/dummy.ts
 rm -f servers/lib/dummy.ts
