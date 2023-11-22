9251f9cdc343ce75213c4465c8e8a77345e2d1a9
lindot11 <lindaspostboks@gmail.com>
2023-11-22 16:21:43 +0100

    test git hooks

--- githook_test.sh
+++ githook_test.sh
@@ -1,7 +1,7 @@
 # 1. test all hooks pass
 printf "Creating dummy files...\n\n"
 echo "const dummy = 'Hello, client';" > client/dummy.ts
-echo "//'Hello, runner';" > servers/execution/runner/dummy.js
+echo "// const dummy = 'Hello, runner'; var a = dummy;" > servers/execution/runner/dummy.js
 echo "# Hello, lib" > servers/lib/dummy.md
 
 printf "Testing all hooks pass...\n\n"
