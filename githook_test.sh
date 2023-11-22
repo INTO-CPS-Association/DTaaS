fe1f57980023c28169a3e5d75069d300b8daaea6
lindot11 <lindaspostboks@gmail.com>
2023-11-22 16:05:51 +0100

    githook test and log for documentation

--- githook_test.sh
+++ githook_test.sh
@@ -1,27 +1,4 @@
-TOP_DIR="$(pwd)"
-
-# 1. initial setup for testing
-# printf "Cleaning...\n\n"
-# cd client
-# yarn clean
-# cd "$TOP_DIR"
-
-# cd servers/execution/runner
-# yarn clean
-# cd "$TOP_DIR"
-
-# cd servers/lib
-# yarn clean
-# cd "$TOP_DIR"
-
-# rm -rf .git/hooks/
-
-# printf "Installing...\n\n"
-# pip install pre-commit
-# pre-commit autoupdate
-# pre-commit install
-
-# 2. test all hooks pass
+# 1. test all hooks pass
 # printf "Creating dummy files...\n\n"
 # echo "const dummy = 'Hello, client';" > client/dummy.ts
 # echo "const dummy = 'Hello, runner';" > servers/execution/runner/dummy.js
@@ -32,7 +9,7 @@ TOP_DIR="$(pwd)"
 # git commit -m "test git hooks"
 # git push
 
-# 3. test prettier hook
+# 2. test prettier hook fail
 # printf "Creating dummy files...\n\n"
 # echo "const dummy   = 'Hello, client';" > client/dummy.ts
 
@@ -41,7 +18,7 @@ TOP_DIR="$(pwd)"
 # git commit -m "test prettier hooks"
 # git reset
 
-# 4. test eslint hook fail
+# 3. test eslint hook fail
 # printf "Creating dummy files...\n\n"
 # echo "const dummy = 'Hello, runner';" > servers/execution/runner/dummy.js
 
@@ -50,22 +27,21 @@ TOP_DIR="$(pwd)"
 # git commit -m "test eslint hook"
 # git reset
 
-# 5. test markdownlint hook fail
-printf "Creating dummy files...\n\n"
-echo " Hello, lib" > servers/lib/dummy.md
+# 4. test markdownlint hook fail
+# printf "Creating dummy files...\n\n"
+# echo " Hello, lib" > servers/lib/dummy.md
 
-printf "Testing markdownlint hoot...\n\n"
-git add .
-git commit -m "test markdownlint hook"
-git reset
+# printf "Testing markdownlint hoot...\n\n"
+# git add .
+# git commit -m "test markdownlint hook"
+# git reset
 
-# remove
+# clean up after each test
 printf "Removing dummy files...\n\n"
 rm -f client/dummy.ts
 rm -f servers/execution/runner/dummy.js
 rm -f servers/lib/dummy.md
 
-# reset
 printf "Resetting...\n\n"
 git add .
 git commit -m "reset after test"
