ece23b05c378004fdb950cebb30bde1a08fca0bb
prasadtalasila <prasad.talasila@gmail.com>
2023-11-27 12:13:10 +0100

    Make some yarn commands cross-platform

--- client/script/graph.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PATH="$(yarn bin):$PATH"
-export PATH
-printf "Generate dependency graph for code"
-
-madge --image src.svg src
-madge --image test.svg test
--- client/script/start.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-printf "starting the node application"
-#node build/index.js
-
-# serve static site; can be run from production server
-# serve -s <directory> -l <port>
-serve -s build -l 4000
