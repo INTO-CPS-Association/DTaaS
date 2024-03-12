0bbc9e6413c9531e3f1bc1c18a42978ba1422d6a
prasadtalasila <prasad.talasila@gmail.com>
2024-03-12 17:56:37 +0100

    Fix linting mistakes

--- servers/execution/runner/src/config/configuration.ts
+++ servers/execution/runner/src/config/configuration.ts
@@ -4,9 +4,8 @@ import { join } from 'path';
 import Config from './Config.interface';
 
 const YAML_CONFIG_FILENAME = 'runner.yaml';
-const __dirname = process.cwd();
 
 export default () =>
   yaml.load(
-    readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
+    readFileSync(join(process.cwd(), YAML_CONFIG_FILENAME), 'utf8'),
   ) as Record<string, Config>;
