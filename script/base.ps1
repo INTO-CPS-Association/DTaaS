533f33ae9f0ddc24867b22af29baf30732c1fccc
Aryan Bhosale <aryan.bhosale1719@gmail.com>
2024-03-28 13:54:05 +0530

    fixed errors in base.ps1

--- script/base.ps1
+++ script/base.ps1
@@ -1,8 +1,7 @@
 # Install Chocolatey if not already installed
 if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
     Set-ExecutionPolicy Bypass -Scope Process -Force
-    $installDir = Join-Path $env:USERPROFILE "choco"
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1")) -installDirectory $installDir
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
 # Install Git if not already installed
@@ -13,50 +12,60 @@ if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
 # Update and upgrade system
 choco upgrade all -y
 
-# Install docker for containers and microservices
-# https://docs.docker.com/engine/install/ubuntu/
-choco install -y docker-desktop
+# Check if docker-desktop is already installed
+if (-not (Get-Command docker-desktop -ErrorAction SilentlyContinue)) {
+    # Install docker for containers and microservices
+    # https://docs.docker.com/engine/install/ubuntu/
+    choco install -y docker-desktop
 
-# Add user to docker group
-$user = $env:UserName
-$dockerGroup = 'docker'
-if ($null -eq (Get-LocalGroupMember -Group $dockerGroup -Member $user)) {
-    Add-LocalGroupMember -Group $dockerGroup -Member $user
+    # Check if docker group exists, if not, create it
+    $dockerGroup = 'docker'
+    if (-not (Get-LocalGroup -Name $dockerGroup -ErrorAction SilentlyContinue)) {
+        New-LocalGroup -Name $dockerGroup
+    }
+
+    # Add user to docker group if not already a member
+    $user = $env:UserName
+    if (-not (Get-LocalGroupMember -Group $dockerGroup -Member $user)) {
+        Add-LocalGroupMember -Group $dockerGroup -Member $user
+    }
+
+    # Make docker available to the user account
+    Write-Host "`n`n`nMake docker available to your user account....`n......."
+    Write-Host "Your user account is: $user"
+    Write-Host "If your user account is a member of the docker group, let the installation script continue."
+    Write-Host "Otherwise, exit this script and run the following command:`n"
+    Write-Host "Add-LocalGroupMember -Group docker -Member $user`n"
+    Write-Host "Log out and log in again. You can run this script again after login.`n"
+
+    # Wait for user action
+    Write-Host "Press Ctl+C if you need to complete this task...."
+    Write-Host "Waiting for 60 seconds...."
+    Start-Sleep -Seconds 60
+
+    # Start Docker service
+    Start-Service -Name "com.docker.service"
+
+    # Test Docker installation
+    docker run hello-world
+
+    # Enable Docker service
+    Set-Service -Name "com.docker.service" -StartupType Automatic
+
+    # Install docker-compose from https://docs.docker.com/compose/install/other/
+    $url = "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-windows-x86_64.exe"
+    $outputPath = Join-Path $env:ProgramFiles "Docker\docker-compose.exe"
+    Invoke-WebRequest -Uri $url -OutFile $outputPath
+    # Ensure docker-compose is in the system path
+    [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$outputPath", [EnvironmentVariableTarget]::Machine)
 }
 
-# Make docker available to the user account
-Write-Host "`n`n`nMake docker available to your user account....`n......."
-Write-Host "Your user account is a member of:"
-Write-Host (Get-LocalGroupMember -Group $user | Select-Object Name)
-Write-Host "If your user account is a member of the docker group, let the installation script continue."
-Write-Host "Otherwise, exit this script and run the following command:`n"
-Write-Host "Add-LocalGroupMember -Group docker -Member $user`n"
-Write-Host "Log out and log in again. You can run this script again after login.`n"
-
-# Wait for user action
-Write-Host "Press Ctl+C if you need to complete this task...."
-Write-Host "Waiting for 60 seconds...."
-Start-Sleep -Seconds 60
-
-# Start Docker service
-Start-Service -Name "com.docker.service"
-
-# Test Docker installation
-docker run hello-world
-
-# Enable Docker service
-Set-Service -Name "com.docker.service" -StartupType Automatic
-
-# Install docker-compose from https://docs.docker.com/compose/install/other/
-$url = "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-windows-x86_64.exe"
-$outputPath = Join-Path $env:ProgramFiles "Docker\docker-compose.exe"
-Invoke-WebRequest -Uri $url -OutFile $outputPath
-# Ensure docker-compose is in the system path
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$outputPath", [EnvironmentVariableTarget]::Machine)
-
-# Install Node.js environment
-choco install -y nodejs-lts
-npm install -g npm@10.2.0
+# Check if Node.js is already installed
+if (-not (Get-Command node -ErrorAction SilentlyContinue)) {
+    # Install Node.js environment if not already installed
+    choco install -y nodejs-lts
+    npm install -g npm@10.2.0
+}
 
 # Install Yarn
 choco install -y yarn
