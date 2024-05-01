21529003710e23ac3d340fb0f4bda68915f7b277
prasadtalasila <prasad.talasila@gmail.com>
2024-05-01 09:56:34 +0000

    Adds tests for commander. WIP

--- servers/execution/runner/test/e2e/commander.spec.ts
+++ servers/execution/runner/test/e2e/commander.spec.ts
@@ -1,10 +1,38 @@
+import { jest } from '@jest/globals';
 import CLI, { createCommand } from 'src/config/commander';
 import Keyv from 'keyv';
 
 describe('Commander functionality', () => {
-  it('Should run without any flags', async () => {
-    const [program, CLIOptions] = createCommand('runner');
-    await CLI(program, CLIOptions);
-    expect(CLIOptions).toBeInstanceOf(Keyv);
+
+  it('Should invoke commander correctly', async () => {
+    const [program, CLIOptionsExp] = createCommand('runner');
+    const argvCall = jest.spyOn(program, 'parse');
+
+    await CLI(program, CLIOptionsExp);
+
+    expect(argvCall).toHaveBeenCalled();
+    expect(program.opts()).toEqual({
+      "config": "runner.yaml"
+    });
   });
+
+  it.skip('Should process flags correctly', async () => {
+    const [program, CLIOptionsExp] = createCommand('runner');
+    jest.mock('process', () => ({
+      argv: ['node', 'main.js', '--config', 'runner.yaml.test'],
+    }));
+    await CLI(program, CLIOptionsExp);
+
+    expect(program.opts()).toEqual({
+      "config": "runner.yaml.test"
+    });
+  });
+
+  it('Should run without any flags', async () => {
+    const [program, CLIOptionsExp] = createCommand('runner');
+    const CLIOptions = await CLI(program, CLIOptionsExp);
+    expect(CLIOptions).toBeInstanceOf(Keyv);
+    expect(CLIOptionsExp).toEqual(CLIOptions);
+  });
+  
 });
