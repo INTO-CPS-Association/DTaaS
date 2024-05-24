512deebe53ae07377350497be90cf8701062866d
raiiasingh19 <raiia.singh1@gmail.com>
2024-05-24 12:31:41 +0530

    fetch err fix

--- client/src/util/auth/Authentication.ts
+++ client/src/util/auth/Authentication.ts
@@ -23,13 +23,11 @@ export function getAndSetUsername(auth: CustomAuthContext) {
 }
 
 function clearCookies() {
-  const cookies = document.cookie.split(';');
-
-  for (const cookie of cookies) {
+  document.cookie.split(';').forEach(cookie => {
     const eqPos = cookie.indexOf('=');
     const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
     document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
-  }
+  });
 }
 
 export async function signOut() {
