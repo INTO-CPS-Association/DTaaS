26c7c4ec2ad103f52b96382300d0117b68134d6a
atomicgamedev <enokmalik@gmail.com>
2024-06-20 19:54:45 +0200

    Add sessionStorage clear, fetch and error tests

--- client/test/unitTests/Util/Auth/Authentication.test.ts
+++ client/test/unitTests/Util/Auth/Authentication.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { signOut } from 'util/auth/Authentication';
 import { useAuth } from 'react-oidc-context';
 import { getLogoutRedirectURI } from 'util/envUtil';
@@ -9,31 +10,72 @@ jest.mock('util/envUtil', () => ({
 
 describe('signOut', () => {
     const user = { idToken: "token" };
-    const signOutSilent = jest.fn();
+    const signoutSilent = jest.fn();
+    const clear = jest.fn();
+    Object.defineProperty(global, 'fetch', {
+        value: jest.fn(async (URL) => {
+            switch (URL) {
+                case "https://example.com/_oauth/logout":
+                    return {
+                        ok: true,
+                        status: 401,
+                        json: async () => { }
+                    };
+                default: {
+                    throw new Error(`Unhandled request: ${URL}`);
+                }
+            }
+        }),
+        writable: true
+    });
 
     beforeEach(() => {
         (useAuth as jest.Mock).mockReturnValue({
-            signOutSilent: signOutSilent,
+            signoutSilent: signoutSilent,
             user: user
         });
     });
 
     afterEach(() => { jest.clearAllMocks(); })
 
-    it('does not signOutSilent if auth.user is null', async () => {
+    it('does not signoutSilent if auth.user is null', async () => {
         const auth = useAuth();
         auth.user = null;
 
         const signOutResult = await signOut(auth);
 
         expect(signOutResult).toBeUndefined();
-        expect(signOutSilent).not.toHaveBeenCalled();
+        expect(signoutSilent).not.toHaveBeenCalled();
     });
 
-    it('thows an error if the logout redirect URI does not exist', async () => {
+    it('fetches the URI from getLogoutRedirectURI', async () => {
         const auth = useAuth();
+        (getLogoutRedirectURI as jest.Mock).mockReturnValue("https://example.com/");
+        await signOut(auth);
+        expect(global.fetch).toHaveBeenCalledWith("https://example.com/_oauth/logout");
+    });
 
-        (getLogoutRedirectURI as jest.Mock).mockReturnValue("");
-        await expect(signOut(auth)).rejects.toThrow();
+    it('throws an error if the logout redirect URI does not exist', async () => {
+        const auth = useAuth();
+        (getLogoutRedirectURI as jest.Mock).mockReturnValue("foo.com");
+        try {
+            await signOut(auth);
+        } catch (e) {
+            expect(global.fetch).toHaveBeenCalled();
+            expect(e).toBeInstanceOf(Error);
+        }
+    });
+
+    it('clears sessionStorage', async () => {
+        const auth = useAuth();
+        Object.defineProperty(window, 'sessionStorage', {
+            value: {
+                clear: clear,
+            },
+            writable: true
+        });
+        await signOut(auth);
+
+        expect(clear).toHaveBeenCalled();
     });
 })
