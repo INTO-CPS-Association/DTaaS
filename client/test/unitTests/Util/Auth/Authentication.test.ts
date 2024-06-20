9beb3b4a2c87c82103b383e54ebff4ebe013a05c
atomicgamedev <enokmalik@gmail.com>
2024-06-20 20:58:57 +0200

    Add negative signOutSilent test

--- client/test/unitTests/Util/Auth/Authentication.test.ts
+++ client/test/unitTests/Util/Auth/Authentication.test.ts
@@ -48,11 +48,30 @@ describe('signOut', () => {
         expect(signoutSilent).not.toHaveBeenCalled();
     });
 
+    it('signsOutSilent if user is authorized', async () => {
+        const auth = useAuth();
+        (getLogoutRedirectURI as jest.Mock).mockReturnValue("https://example.com/");
+        await signOut(auth);
+        expect(signoutSilent).toHaveBeenCalled();
+    });
+
     it('fetches the URI from getLogoutRedirectURI', async () => {
         const auth = useAuth();
         (getLogoutRedirectURI as jest.Mock).mockReturnValue("https://example.com/");
         await signOut(auth);
         expect(global.fetch).toHaveBeenCalledWith("https://example.com/_oauth/logout");
+    });
+
+    it('reloads the page', async () => {
+        const auth = useAuth();
+        (getLogoutRedirectURI as jest.Mock).mockReturnValue("https://example.com/");
+        Object.defineProperty(window, "location", {
+            value: { reload: jest.fn() },
+            writable: true,
+        })
+        await signOut(auth);
+
+        expect(window.location.reload).toHaveBeenCalled();
     });
 
     it('throws an error if the logout redirect URI does not exist', async () => {
@@ -64,6 +83,12 @@ describe('signOut', () => {
             expect(global.fetch).toHaveBeenCalled();
             expect(e).toBeInstanceOf(Error);
         }
+    });
+
+    it('throws an error if signOutSilent rejects', async () => {
+        const auth = useAuth();
+        auth.signoutSilent = jest.fn().mockRejectedValue(new Error("signOutSilent rejected"));
+        await expect(signOut(auth)).rejects.toThrow(Error("Error occurred during logout: Error: signOutSilent rejected"));
     });
 
     it('clears sessionStorage', async () => {
