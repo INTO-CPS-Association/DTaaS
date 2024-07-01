454a1c19f2e89e86cb3db44831b336f0f82060f7
atomicgamedev <enokmalik@gmail.com>
2024-07-01 19:42:45 +0200

    Add new group list texts and tests

--- client/src/route/auth/AccountTabData.tsx
+++ client/src/route/auth/AccountTabData.tsx
@@ -4,30 +4,45 @@ import { TabData } from 'components/tab/subcomponents/TabRender';
 
 function ProfileTab() {
   const { user } = useAuth();
-  const name = (user?.profile.preferred_username as string | undefined) ?? "";
+  const name = (user?.profile.preferred_username as string | undefined) ?? '';
   const pfp = user?.profile.picture;
   const profileUrl = user?.profile.profile;
-  const groups = ((user?.profile.groups as string[] | undefined) ?? []);
+  const groups = (user?.profile.groups as string[] | undefined) ?? [];
 
-  const boldName = React.createElement("b", null, name);
-  const boldGroups = groups.map(group => React.createElement("b", null, group));
+  const boldName = React.createElement('b', null, name);
+  const boldGroups = groups.map((group) =>
+    React.createElement('b', null, group),
+  );
   const groupListing: React.ReactNode[] = [];
-  boldGroups.slice(0, -1).forEach((groupElement, i) => {
-    const isSecondToLastGroup = (i < boldGroups.length - 2);
-    const seperator = isSecondToLastGroup ? ", " : " and "
-    groupListing.push(groupElement, seperator);
-  });
-  groupListing.push(boldGroups.slice(-1));
-  const groupParagraph = React.createElement("p", null, boldName, " belongs to ", groupListing);
+  boldGroups.slice(0, -1).map(groupElement => groupListing.push(groupElement, ', '))
+  if (groups.length != 1) {
+    groupListing.splice(groupListing.length - 1, 1, [' and ', boldGroups.slice(-1)]);
+  } else {
+    groupListing.push(boldGroups);
+  }
+  const groupSuffix = (groupListing.length > 1) ? 's' : '';
+  const paragraphText = React.Children.toArray((groups.length > 0) ? [boldName,
+    ' belongs to ',
+    groupListing,
+    ` group${groupSuffix}.`] : [boldName, ' does not belong to any groups.']);
+
+  const groupParagraph = React.createElement(
+    'p',
+    null,
+    paragraphText
+  );
 
   return (
     <div>
       <h2>Profile</h2>
       <img src={pfp} data-testid="profile-picture" />
       <p>
-        The username is {boldName}. See more details on the user on <b><a href={profileUrl}>SSO OAuth Provider.</a></b>
+        The username is {boldName}. See more details on {' '}
+        <b>
+          <a href={profileUrl}>SSO OAuth Provider.</a>
+        </b>
       </p>
-      {groups && groupParagraph}
+      {groupParagraph}
     </div>
   );
 }
@@ -37,11 +52,15 @@ function SettingsTab() {
   return (
     <div>
       <h2>Settings</h2>
-      <p>Edit the profile on <b><a href={profileUrl}>SSO OAuth Provider.</a></b></p>
+      <p>
+        Edit the profile on{' '}
+        <b>
+          <a href={profileUrl}>SSO OAuth Provider.</a>
+        </b>
+      </p>
     </div>
   );
 }
-
 
 const tabs: TabData[] = [
   {
--- client/test/unitTests/Routes/Account.test.tsx
+++ client/test/unitTests/Routes/Account.test.tsx
@@ -1,12 +1,97 @@
 import * as React from 'react';
 import Account from 'route/auth/Account';
-import tabs from 'route/auth/AccountTabData';
-import { InitRouteTests, itDisplaysContentOfTabs } from '../testUtils';
+import { render, screen } from '@testing-library/react';
+import { useAuth } from 'react-oidc-context';
 
-describe('Account Page', () => {
-  const tabLabels: string[] = [];
-  tabs.forEach((tab) => tabLabels.push(tab.label));
-  InitRouteTests(<Account />);
+jest.mock('react-oidc-context');
 
-  itDisplaysContentOfTabs(tabs);
+function testStaticElements() {
+  render(
+    <Account />
+  );
+
+  const profilePicture = screen.getByTestId('profile-picture');
+  expect(profilePicture).toBeInTheDocument();
+  expect(profilePicture).toHaveAttribute('src', 'pfp.jpg');
+
+  const username = screen.getAllByText('user1');
+  expect(username).not.toBeNull();
+  expect(username).toHaveLength(2);
+
+  const profileLink = screen.getByRole('link', { name: /SSO OAuth Provider/i });
+  expect(profileLink).toBeInTheDocument();
+  expect(profileLink).toHaveAttribute('href', 'test.com');
+}
+
+describe('AccountTabs', () => {
+  const mockProfile = {
+    preferred_username: "user1",
+    picture: "pfp.jpg",
+    profile: "test.com",
+    groups: ['']
+  };
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
+
+  test('renders AccountTabs with correct profile information when user is in 0 groups', () => {
+    mockProfile.groups = [];
+    const mockuser = { profile: mockProfile };
+    (useAuth as jest.Mock).mockReturnValue({
+      user: mockuser,
+    });
+    testStaticElements();
+
+    const groupInfo = screen.getByText(/belong to/);
+    expect(groupInfo).toHaveProperty('innerHTML', '<b>user1</b> does not belong to any groups.');
+  });
+
+  test('renders AccountTabs with correct profile information when user is in 1 group', () => {
+    mockProfile.groups = ["group-one"];
+    const mockuser = { profile: mockProfile };
+    (useAuth as jest.Mock).mockReturnValue({
+      user: mockuser,
+    });
+    testStaticElements();
+
+    const groupInfo = screen.getByText(/belongs to/);
+    expect(groupInfo).toHaveProperty('innerHTML', '<b>user1</b> belongs to <b>group-one</b> group.');
+  });
+
+  test('renders AccountTabs with correct profile information when user is in 2 groups', () => {
+    mockProfile.groups = ["first-group", "second-group"];
+    const mockuser = { profile: mockProfile };
+    (useAuth as jest.Mock).mockReturnValue({
+      user: mockuser,
+    });
+    testStaticElements();
+
+    const groupInfo = screen.getByText(/belongs to/);
+    expect(groupInfo).toHaveProperty('innerHTML', '<b>user1</b> belongs to <b>first-group</b> and <b>second-group</b> groups.');
+  });
+
+  test('renders AccountTabs with correct profile information when user is in 3 groups', () => {
+    mockProfile.groups = ["group1", "group2", "group3"];
+    const mockuser = { profile: mockProfile };
+    (useAuth as jest.Mock).mockReturnValue({
+      user: mockuser,
+    });
+    testStaticElements();
+
+    const groupInfo = screen.getByText(/belongs to/);
+    expect(groupInfo).toHaveProperty('innerHTML', '<b>user1</b> belongs to <b>group1</b>, <b>group2</b> and <b>group3</b> groups.');
+  });
+
+  test('renders AccountTabs with correct profile information when user is in more than 3 groups', () => {
+    mockProfile.groups = ["g1", "g2", "g3", "g4", "g5"];
+    const mockuser = { profile: mockProfile };
+    (useAuth as jest.Mock).mockReturnValue({
+      user: mockuser,
+    });
+    testStaticElements();
+
+    const groupInfo = screen.getByText(/belongs to/);
+    expect(groupInfo).toHaveProperty('innerHTML', '<b>user1</b> belongs to <b>g1</b>, <b>g2</b>, <b>g3</b>, <b>g4</b> and <b>g5</b> groups.');
+  });
 });
