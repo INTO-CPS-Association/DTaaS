eb578afecaa3dd082fc0c738fb5c68e8e6329c98
atomicgamedev <enokmalik@gmail.com>
2024-07-01 21:32:20 +0200

    Refactor ProfileTab

--- client/src/route/auth/AccountTabData.tsx
+++ client/src/route/auth/AccountTabData.tsx
@@ -2,42 +2,55 @@ import * as React from 'react';
 import { useAuth } from 'react-oidc-context';
 import { TabData } from 'components/tab/subcomponents/TabRender';
 
+function ListGroups(groups: string[]): React.ReactNode[] {
+  const boldGroups = groups.map((group) =>
+    React.createElement('b', null, group),
+  );
+
+  const userBelongsToOneGroup = (groups.length == 1);
+  if (userBelongsToOneGroup) {
+    return boldGroups;
+  }
+
+  const groupListing: React.ReactNode[] = [];
+  boldGroups.slice(0, -1).map(groupElement => groupListing.push(groupElement, ', '))
+  groupListing.splice(groupListing.length - 1, 1, [' and ', boldGroups.slice(-1)]);
+  return groupListing;
+}
+
+function GroupParagraph(groups: string[], name: React.ReactNode) {
+  const userBelongsToAnyGroups = (groups.length > 0);
+  if (!userBelongsToAnyGroups) {
+    return (
+      <p>
+        <b>{name}</b> does not belong to any groups.
+      </p>
+    );
+  }
+
+  const groupListing = ListGroups(groups);
+  const groupSuffix = (groups.length > 1) ? 's' : '';
+  return (
+    <p>
+      <b>{name}</b> belongs to {React.Children.toArray(groupListing)} group{groupSuffix}.
+    </p>
+  );
+}
+
 function ProfileTab() {
   const { user } = useAuth();
   const name = (user?.profile.preferred_username as string | undefined) ?? '';
   const pfp = user?.profile.picture;
   const profileUrl = user?.profile.profile;
   const groups = (user?.profile.groups as string[] | undefined) ?? [];
-
-  const boldName = React.createElement('b', null, name);
-  const boldGroups = groups.map((group) =>
-    React.createElement('b', null, group),
-  );
-  const groupListing: React.ReactNode[] = [];
-  boldGroups.slice(0, -1).map(groupElement => groupListing.push(groupElement, ', '))
-  if (groups.length != 1) {
-    groupListing.splice(groupListing.length - 1, 1, [' and ', boldGroups.slice(-1)]);
-  } else {
-    groupListing.push(boldGroups);
-  }
-  const groupSuffix = (groupListing.length > 1) ? 's' : '';
-  const paragraphText = React.Children.toArray((groups.length > 0) ? [boldName,
-    ' belongs to ',
-    groupListing,
-    ` group${groupSuffix}.`] : [boldName, ' does not belong to any groups.']);
-
-  const groupParagraph = React.createElement(
-    'p',
-    null,
-    paragraphText
-  );
+  const groupParagraph = GroupParagraph(groups, name);
 
   return (
     <div>
       <h2>Profile</h2>
       <img src={pfp} data-testid="profile-picture" />
       <p>
-        The username is {boldName}. See more details on {' '}
+        The username is <b>{name}</b>. See more details on {' '}
         <b>
           <a href={profileUrl}>SSO OAuth Provider.</a>
         </b>
