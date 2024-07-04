9bd4c1b735ac7cc2b9cac02910bccea72eebfc6b
atomicgamedeveloper <109801255+atomicgamedeveloper@users.noreply.github.com>
2024-07-04 15:57:56 +0200

    Update Account tabs and add group list (#838)
     - Uses the existing user profile information obtained
       in OAuth signin process and updates the profile page.
     - Downgrades the ESLint version to make it compatible
       with eslintrc configuration file.

--- client/src/route/auth/Account.tsx
+++ client/src/route/auth/Account.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Layout from 'page/Layout';
-import { Typography } from '@mui/material';
 import TabComponent from 'components/tab/TabComponent';
 import { TabData } from 'components/tab/subcomponents/TabRender';
 import tabs from './AccountTabData';
@@ -8,7 +7,7 @@ import tabs from './AccountTabData';
 function AccountContent() {
   const AccountTab: TabData[] = tabs.map((tab) => ({
     label: tab.label,
-    body: <Typography variant="body1">{tab.body}</Typography>,
+    body: tab.body,
   }));
 
   return (
