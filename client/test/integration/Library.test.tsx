a57a3e235506fa3d2f22bba08c4c1e3e1fe53768
atomicgamedev <enokmalik@gmail.com>
2024-07-08 20:38:09 +0200

    Format and add test for selection attribute

--- client/test/integration/Library.test.tsx
+++ client/test/integration/Library.test.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import {
   fireEvent,
   getDefaultNormalizer,
-  render, screen, within
+  render,
+  screen,
+  within,
 } from '@testing-library/react';
 import { useAuth } from 'react-oidc-context';
 import Library from '../../src/route/library/Library';
-import { assetType, scope } from '../../src/route/library/LibraryTabData'
+import { assetType, scope } from '../../src/route/library/LibraryTabData';
 
 jest.mock('react-oidc-context', () => ({
   useAuth: jest.fn(),
@@ -22,7 +24,10 @@ jest.mock('../../src/util/auth/Authentication', () => ({
 }));
 
 describe('Library', () => {
-  const normalizer = getDefaultNormalizer({ trim: false, collapseWhitespace: false });
+  const normalizer = getDefaultNormalizer({
+    trim: false,
+    collapseWhitespace: false,
+  });
 
   beforeEach(() => {
     const userMock = {
@@ -40,7 +45,7 @@ describe('Library', () => {
   afterEach(() => {
     jest.restoreAllMocks();
     jest.clearAllMocks();
-  })
+  });
 
   it('renders the Library content correctly', () => {
     const tablists = screen.getAllByRole('tablist');
@@ -51,27 +56,52 @@ describe('Library', () => {
     const assetTypesTabs = within(assetTypeTablist).getAllByRole('tab');
     expect(assetTypesTabs).toHaveLength(5);
 
-    const functionsTab = within(assetTypeTablist).getByRole('tab', { name: "Functions", selected: true });
+    const functionsTab = within(assetTypeTablist).getByRole('tab', {
+      name: 'Functions',
+      selected: true,
+    });
     expect(functionsTab).toBeInTheDocument();
-    const modelsTab = within(assetTypeTablist).getByRole('tab', { name: "Models", selected: false });
+    const modelsTab = within(assetTypeTablist).getByRole('tab', {
+      name: 'Models',
+      selected: false,
+    });
     expect(modelsTab).toBeInTheDocument();
-    const toolsTab = within(assetTypeTablist).getByRole('tab', { name: "Tools", selected: false });
+    const toolsTab = within(assetTypeTablist).getByRole('tab', {
+      name: 'Tools',
+      selected: false,
+    });
     expect(toolsTab).toBeInTheDocument();
-    const dataTab = within(assetTypeTablist).getByRole('tab', { name: "Data", selected: false });
+    const dataTab = within(assetTypeTablist).getByRole('tab', {
+      name: 'Data',
+      selected: false,
+    });
     expect(dataTab).toBeInTheDocument();
-    const digitalTwinsTab = within(assetTypeTablist).getByRole('tab', { name: "Digital Twins", selected: false });
+    const digitalTwinsTab = within(assetTypeTablist).getByRole('tab', {
+      name: 'Digital Twins',
+      selected: false,
+    });
     expect(digitalTwinsTab).toBeInTheDocument();
 
     let i = 0;
-    const functionsParagraph = screen.getByText(assetType[i++].body, { normalizer: normalizer });
+    const functionsParagraph = screen.getByText(assetType[i++].body, {
+      normalizer,
+    });
     expect(functionsParagraph).toBeInTheDocument();
-    const modelsParagraph = screen.queryByText(assetType[i++].body, { normalizer: normalizer });
+    const modelsParagraph = screen.queryByText(assetType[i++].body, {
+      normalizer,
+    });
     expect(modelsParagraph).not.toBeInTheDocument();
-    const toolsParagraph = screen.queryByText(assetType[i++].body, { normalizer: normalizer });
+    const toolsParagraph = screen.queryByText(assetType[i++].body, {
+      normalizer,
+    });
     expect(toolsParagraph).not.toBeInTheDocument();
-    const dataParagraph = screen.queryByText(assetType[i++].body, { normalizer: normalizer });
+    const dataParagraph = screen.queryByText(assetType[i++].body, {
+      normalizer,
+    });
     expect(dataParagraph).not.toBeInTheDocument();
-    const digitalTwinsParagraph = screen.queryByText(assetType[i++].body, { normalizer: normalizer });
+    const digitalTwinsParagraph = screen.queryByText(assetType[i++].body, {
+      normalizer,
+    });
     expect(digitalTwinsParagraph).not.toBeInTheDocument();
 
     const scopeTablist = tablists[1];
@@ -79,42 +109,64 @@ describe('Library', () => {
     const scopeTabs = within(scopeTablist).getAllByRole('tab');
     expect(scopeTabs).toHaveLength(2);
 
-    const privateTab = within(scopeTablist).getByRole('tab', { name: "Private", selected: true });
+    const privateTab = within(scopeTablist).getByRole('tab', {
+      name: 'Private',
+      selected: true,
+    });
     expect(privateTab).toBeInTheDocument();
-    const commonTab = within(scopeTablist).getByRole('tab', { name: "Common", selected: false });
+    const commonTab = within(scopeTablist).getByRole('tab', {
+      name: 'Common',
+      selected: false,
+    });
     expect(commonTab).toBeInTheDocument();
 
     i = 0;
-    const privateParagraph = screen.getByText(scope[i++].body, { normalizer: normalizer });
+    const privateParagraph = screen.getByText(scope[i++].body, {
+      normalizer,
+    });
     expect(privateParagraph).toBeInTheDocument();
 
-    const commonParagraph = screen.queryByText(scope[i++].body, { normalizer: normalizer });
+    const commonParagraph = screen.queryByText(scope[i++].body, {
+      normalizer,
+    });
     expect(commonParagraph).not.toBeInTheDocument();
 
-    const iframe = screen.getByTitle("Functions");
+    const iframe = screen.getByTitle('Functions');
     expect(iframe).toBeInTheDocument();
-    expect(iframe).toHaveProperty('src', 'https://example.com/URL_LIBtree/functions')
+    expect(iframe).toHaveProperty(
+      'src',
+      'https://example.com/URL_LIBtree/functions',
+    );
   });
 
-  it('updates text and iframe src when Models tab is pressed', () => {
+  it('updates text and iframe src when Models tab is selected', () => {
     const tablists = screen.getAllByRole('tablist');
     const assetTypeTablist = tablists[0];
-    const modelsTab = within(assetTypeTablist).getByRole('tab', { name: 'Models', selected: false });
+    const modelsTab = within(assetTypeTablist).getByRole('tab', {
+      name: 'Models',
+      selected: false,
+    });
     expect(modelsTab).toBeInTheDocument();
 
     const modelsParagraph = screen.queryByText(assetType[1].body);
     expect(modelsParagraph).not.toBeInTheDocument();
 
     fireEvent.click(modelsTab);
-    const modelsTabAfterClick = within(assetTypeTablist).getByRole('tab', { name: 'Models', selected: true });
+    const modelsTabAfterClick = within(assetTypeTablist).getByRole('tab', {
+      name: 'Models',
+      selected: true,
+    });
     expect(modelsTabAfterClick).toBeInTheDocument();
 
     const modelsParagraphAfterClick = screen.getByText(assetType[1].body);
     expect(modelsParagraphAfterClick).toBeInTheDocument();
 
-    const iframe = screen.getByTitle("Models");
+    const iframe = screen.getByTitle('Models');
     expect(iframe).toBeInTheDocument();
-    expect(iframe).toHaveProperty('src', 'https://example.com/URL_LIBtree/models')
+    expect(iframe).toHaveProperty(
+      'src',
+      'https://example.com/URL_LIBtree/models',
+    );
   });
 
   it('does not update text and iframe src when clicking default assetType tab', () => {
@@ -124,21 +176,33 @@ describe('Library', () => {
     const tabLabel = assetType[0].label;
     const tabBody = assetType[0].body;
 
-    const tab = within(assetTypeTablist).getByRole('tab', { name: tabLabel, selected: true });
+    const tab = within(assetTypeTablist).getByRole('tab', {
+      name: tabLabel,
+      selected: true,
+    });
     expect(tab).toBeInTheDocument();
 
-    const modelsParagraph = screen.queryByText(tabBody, { normalizer: normalizer });
-    expect(modelsParagraph).toBeInTheDocument();
+    const tabParagraph = screen.queryByText(tabBody, {
+      normalizer,
+    });
+    expect(tabParagraph).toBeInTheDocument();
 
     fireEvent.click(tab);
-    const tabAfterClick = within(assetTypeTablist).getByRole('tab', { name: tabLabel, selected: true });
+    const tabAfterClick = within(assetTypeTablist).getByRole('tab', {
+      name: tabLabel,
+      selected: true,
+    });
     expect(tabAfterClick).toBeInTheDocument();
+    expect(tabAfterClick).toBe(tab);
 
-    const tabParagraphAfterClick = screen.getByText(tabBody, { normalizer: normalizer });
+    const tabParagraphAfterClick = screen.getByText(tabBody, {
+      normalizer,
+    });
     expect(tabParagraphAfterClick).toBeInTheDocument();
+    expect(tabParagraphAfterClick).toBe(tabParagraph);
   });
 
-  it('updates text and iframe src according to which assetType tab is pressed', () => {
+  it('updates text and iframe src according to which assetType tab is selected', () => {
     const tablists = screen.getAllByRole('tablist');
     const assetTypeTablist = tablists[0];
     const assetTypesTabs = within(assetTypeTablist).getAllByRole('tab');
@@ -148,23 +212,36 @@ describe('Library', () => {
       const tabLabel = assetType[i].label;
       const tabBody = assetType[i].body;
 
-      const tab = within(assetTypeTablist).getByRole('tab', { name: tabLabel, selected: false });
+      const tab = within(assetTypeTablist).getByRole('tab', {
+        name: tabLabel,
+        selected: false,
+      });
       expect(tab).toBeInTheDocument();
 
-      const modelsParagraph = screen.queryByText(tabBody, { normalizer: normalizer });
-      expect(modelsParagraph).not.toBeInTheDocument();
+      const tabParagraph = screen.queryByText(tabBody, {
+        normalizer,
+      });
+      expect(tabParagraph).not.toBeInTheDocument();
 
       fireEvent.click(tab);
-      const tabAfterClick = within(assetTypeTablist).getByRole('tab', { name: tabLabel, selected: true });
+      const tabAfterClick = within(assetTypeTablist).getByRole('tab', {
+        name: tabLabel,
+        selected: true,
+      });
       expect(tabAfterClick).toBeInTheDocument();
 
-      const tabParagraphAfterClick = screen.getByText(tabBody, { normalizer: normalizer });
+      const tabParagraphAfterClick = screen.getByText(tabBody, {
+        normalizer,
+      });
       expect(tabParagraphAfterClick).toBeInTheDocument();
 
       const iframe = screen.getByTitle(tabLabel);
       expect(iframe).toBeInTheDocument();
-      expect(iframe).toHaveProperty('src', `https://example.com/URL_LIBtree/${tabLabel.replace(" ", "_").toLowerCase()}`);
-    };
+      expect(iframe).toHaveProperty(
+        'src',
+        `https://example.com/URL_LIBtree/${tabLabel.replace(' ', '_').toLowerCase()}`,
+      );
+    }
     expect(spyOnClick).toHaveBeenCalled();
   });
 
@@ -175,21 +252,33 @@ describe('Library', () => {
     const tabLabel = scope[0].label;
     const tabBody = scope[0].body;
 
-    const tab = within(scopeTablist).getByRole('tab', { name: tabLabel, selected: true });
+    const tab = within(scopeTablist).getByRole('tab', {
+      name: tabLabel,
+      selected: true,
+    });
     expect(tab).toBeInTheDocument();
 
-    const modelsParagraph = screen.queryByText(tabBody, { normalizer: normalizer });
-    expect(modelsParagraph).toBeInTheDocument();
+    const tabParagraph = screen.queryByText(tabBody, {
+      normalizer,
+    });
+    expect(tabParagraph).toBeInTheDocument();
 
     fireEvent.click(tab);
-    const tabAfterClick = within(scopeTablist).getByRole('tab', { name: tabLabel, selected: true });
+    const tabAfterClick = within(scopeTablist).getByRole('tab', {
+      name: tabLabel,
+      selected: true,
+    });
     expect(tabAfterClick).toBeInTheDocument();
+    expect(tab).toBe(tabAfterClick);
 
-    const tabParagraphAfterClick = screen.getByText(tabBody, { normalizer: normalizer });
+    const tabParagraphAfterClick = screen.getByText(tabBody, {
+      normalizer,
+    });
     expect(tabParagraphAfterClick).toBeInTheDocument();
+    expect(tabParagraphAfterClick).toBe(tabParagraph);
   });
 
-  it('updates text according to which scope tab is pressed', () => {
+  it('updates text according to which scope tab is selected', () => {
     const tablists = screen.getAllByRole('tablist');
     const scopeTablist = tablists[1];
     const scopesTabs = within(scopeTablist).getAllByRole('tab');
@@ -199,19 +288,58 @@ describe('Library', () => {
       const tabLabel = scope[i].label;
       const tabBody = scope[i].body;
 
-      const tab = within(scopeTablist).getByRole('tab', { name: tabLabel, selected: false });
+      const tab = within(scopeTablist).getByRole('tab', {
+        name: tabLabel,
+        selected: false,
+      });
       expect(tab).toBeInTheDocument();
 
-      const modelsParagraph = screen.queryByText(tabBody, { normalizer: normalizer });
-      expect(modelsParagraph).not.toBeInTheDocument();
+      const tabParagraph = screen.queryByText(tabBody, {
+        normalizer,
+      });
+      expect(tabParagraph).not.toBeInTheDocument();
 
       fireEvent.click(tab);
-      const tabAfterClick = within(scopeTablist).getByRole('tab', { name: tabLabel, selected: true });
+      const tabAfterClick = within(scopeTablist).getByRole('tab', {
+        name: tabLabel,
+        selected: true,
+      });
       expect(tabAfterClick).toBeInTheDocument();
 
-      const tabParagraphAfterClick = screen.getByText(tabBody, { normalizer: normalizer });
+      const tabParagraphAfterClick = screen.getByText(tabBody, {
+        normalizer,
+      });
       expect(tabParagraphAfterClick).toBeInTheDocument();
-    };
+    }
     expect(spyOnClick).toHaveBeenCalled();
+  });
+
+  it('selects the correct tabs when clicking on them', () => {
+    const spyOnClick = jest.spyOn(fireEvent, 'click');
+    const tablists = screen.getAllByRole('tablist');
+
+    const assetTypeTablist = tablists[0];
+    const scopeTablist = tablists[1];
+
+    const assetTypesTabs = within(assetTypeTablist).getAllByRole('tab');
+    const scopeTabs = within(scopeTablist).getAllByRole('tab');
+
+    // Select scope first, then assetType
+    fireEvent.click(scopeTabs[1]);
+    fireEvent.click(assetTypesTabs[2]);
+
+    expect(spyOnClick).toHaveBeenCalledTimes(2);
+
+    expect(assetTypesTabs[2]).toHaveAttribute('aria-selected', 'true');
+    expect(scopeTabs[1]).toHaveAttribute('aria-selected', 'true');
+
+    // Select assetType, then scope
+    fireEvent.click(assetTypesTabs[0]);
+    fireEvent.click(scopeTabs[1]);
+
+    expect(spyOnClick).toHaveBeenCalledTimes(4);
+
+    expect(assetTypesTabs[0]).toHaveAttribute('aria-selected', 'true');
+    expect(scopeTabs[1]).toHaveAttribute('aria-selected', 'true');
   });
 });
