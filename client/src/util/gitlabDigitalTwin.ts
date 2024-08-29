228ed5c394de2ff900c1dd91deb7c74722b76fab
Prasad Talasila <prasadtalasila@users.noreply.github.com>
2024-08-29 12:50:27 +0200

    Adds gitlab API into client codebase (#894)
    - Uses gitbeaker API to showcase gitlab API integration into react client codebase
    - This PR builds on top of PR #830

---
+++ client/src/util/gitlabDigitalTwin.ts
@@ -0,0 +1,58 @@
+import GitlabInstance from './gitlab';
+
+class DigitalTwin {
+  public DTName: string;
+
+  public gitlabInstance: GitlabInstance;
+
+  public lastExecutionStatus: string | null = null;
+
+  constructor(DTName: string, gitlabInstance: GitlabInstance) {
+    this.DTName = DTName;
+    this.gitlabInstance = gitlabInstance;
+  }
+
+  async execute(runnerTag: string): Promise<boolean> {
+    const projectId = await this.gitlabInstance.getProjectId();
+    const triggerToken =
+      projectId && (await this.gitlabInstance.getTriggerToken(projectId));
+
+    if (!projectId || !triggerToken) {
+      this.lastExecutionStatus = 'error';
+      return false;
+    }
+
+    const variables = { DTName: this.DTName, RunnerTag: runnerTag };
+
+    try {
+      await this.gitlabInstance.api.PipelineTriggerTokens.trigger(
+        projectId,
+        'main',
+        triggerToken,
+        { variables },
+      );
+      this.gitlabInstance.logs.push({
+        status: 'success',
+        DTName: this.DTName,
+        runnerTag,
+      });
+      this.lastExecutionStatus = 'success';
+      return true;
+    } catch (error) {
+      this.gitlabInstance.logs.push({
+        status: 'error',
+        error: new Error(String(error)),
+        DTName: this.DTName,
+        runnerTag,
+      });
+      this.lastExecutionStatus = 'error';
+      return false;
+    }
+  }
+
+  executionStatus(): string | null {
+    return this.lastExecutionStatus;
+  }
+}
+
+export default DigitalTwin;
