32b8a7755121ab6d9b3dc4ac23fad3922f74eb61
atomicgamedev <enokmalik@gmail.com>
2024-10-02 19:35:14 +0200

    Refactor useSignOut

--- client/src/util/auth/Authentication.ts
+++ client/src/util/auth/Authentication.ts
@@ -26,34 +26,39 @@ export function useGetAndSetUsername() {
   return getAndSetUsername;
 }
 
+async function performSignOutFlow(auth: AuthContextProps, appURL: string) {
+  const LOGOUT_URL = getLogoutRedirectURI() ?? '';
+  const idToken = auth.user!.id_token;
+
+  await auth.revokeTokens();
+  await auth.removeUser();
+  await auth.clearStaleState();
+
+  sessionStorage.clear();
+  document.cookie = '_xsrf=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+
+  await auth.signoutRedirect({
+    post_logout_redirect_uri: LOGOUT_URL.toString(),
+    id_token_hint: idToken,
+  });
+
+  await fetch(`${cleanURL(appURL)}/_oauth/logout`, {
+    signal: AbortSignal.timeout(30000),
+  });
+
+  setTimeout(() => {
+    window.location.reload();
+  }, 3000);
+}
+
 export function useSignOut() {
   const APP_URL = useAppURL();
-  const CLEAN_APP_URL = cleanURL(APP_URL);
-  const LOGOUT_URL = getLogoutRedirectURI() ?? '';
   const signOut = async (auth: AuthContextProps) => {
     if (!auth.user) {
       return;
     }
-
-    const idToken = auth.user!.id_token;
-
     try {
-      await auth.revokeTokens();
-      await auth.removeUser();
-      await auth.clearStaleState();
-      sessionStorage.clear();
-      document.cookie =
-        '_xsrf=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-      await auth.signoutRedirect({
-        post_logout_redirect_uri: LOGOUT_URL.toString(),
-        id_token_hint: idToken,
-      });
-      await fetch(`${CLEAN_APP_URL}/_oauth/logout`, {
-        signal: AbortSignal.timeout(30000),
-      });
-      setTimeout(() => {
-        window.location.reload();
-      }, 3000);
+      await performSignOutFlow(auth, APP_URL);
     } catch (e) {
       throw new Error(`Error occurred during logout: ${e}`);
     }
