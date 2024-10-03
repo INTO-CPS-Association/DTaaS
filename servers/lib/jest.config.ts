bb58e4bd97dcb392ef791e12351b3b6e6e49f2ff
nichlaes <nichlaes.soerensen@gmail.com>
2024-10-03 11:41:05 +0200

    renamed config

---
+++ servers/lib/jest.config.ts
@@ -0,0 +1,47 @@
+import { createDefaultEsmPreset, type JestConfigWithTsJest } from 'ts-jest';
+
+const jestConfig: JestConfigWithTsJest = {
+  testEnvironment: 'node',
+  transform: {
+    ...createDefaultEsmPreset().transform,
+    '(src|test)/^.+\\.(t|j)sx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  collectCoverage: true,
+  coverageReporters: ['text', 'cobertura', 'clover', 'lcov', 'json'],
+  collectCoverageFrom: ['src/**/*.{ts,js}'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    './dist',
+    '../src/app.module.ts',
+    '../src/main.ts',
+  ],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  modulePathIgnorePatterns: [],
+  coverageDirectory: '<rootDir>/coverage/',
+  coverageThreshold: {
+    global: {
+      branches: 20,
+      functions: 30,
+      lines: 50,
+      statements: 50,
+    },
+  },
+  verbose: true,
+  testRegex: './test/.*\\.spec.tsx?$',
+  modulePaths: ['<rootDir>', '<rootDir>/src/', '<rootDir>/test/'],
+  moduleDirectories: ['node_modules', 'src', 'test'],
+  rootDir: './',
+  roots: ['<rootDir>', 'src/', 'test/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^(\\.\\/.+)\\.js$': '$1',
+  },
+};
+
+export default jestConfig;
