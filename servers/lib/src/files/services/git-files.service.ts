a903716bc716df69c5350b3a7fc2ce3dcd2f7560
nichlaes <nichlaes.soerensen@gmail.com>
2024-10-10 14:35:48 +0200

    fixed eslint errors

--- servers/lib/src/files/services/git-files.service.ts
+++ servers/lib/src/files/services/git-files.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { Project } from 'src/types.js';
 import { IFilesService } from '../interfaces/files.service.interface.js';
 
 @Injectable()
 export default class GitFilesService implements IFilesService {
   // eslint-disable-next-line no-useless-constructor, no-empty-function
-  constructor(private configService: ConfigService) {}
+  constructor() {}
 
-  listDirectory(path: string): Promise<Project> {
+  listDirectory(): Promise<Project> {
     throw new Error('Method not implemented.');
   }
-  readFile(path: string): Promise<Project> {
+  readFile(): Promise<Project> {
     throw new Error('Method not implemented.');
   }
 }
