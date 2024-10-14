409283154e44f7553c39540a2ccc7b851ed54972
nichlaes <nichlaes.soerensen@gmail.com>
2024-10-14 10:43:27 +0200

    added modules

--- servers/lib/src/files/files.module.ts
+++ servers/lib/src/files/files.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import FilesResolver from './resolvers/files.resolver.js';
 import FilesServiceFactory from './services/files-service.factory.js';
-import LocalFilesService from './services/local-files.service.js';
-import GitFilesService from './services/git-files.service.js';
+import { GitFilesModule } from './modules/git-files.module.js';
+import { LocalFilesModule } from './modules/local-files.module.js';
 
 @Module({
-  providers: [FilesResolver, LocalFilesService, FilesServiceFactory, GitFilesService],
+  imports: [
+    LocalFilesModule,
+    GitFilesModule,
+  ],
+  providers: [
+    FilesResolver,
+    FilesServiceFactory,
+  ],
 })
 export default class FilesModule {}
