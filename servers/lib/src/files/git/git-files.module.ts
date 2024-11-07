16b6438474d186702cff0c44c26d607f99c61b49
nichlaes <nichlaes.soerensen@gmail.com>
2024-11-07 13:35:55 +0100

    adds localFileService To git service

--- servers/lib/src/files/git/git-files.module.ts
+++ servers/lib/src/files/git/git-files.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import GitFilesService from './git-files.service.js';
+import LocalFilesService from '../local/local-files.service.js';
 
 @Module({
-  providers: [GitFilesService],
+  providers: [GitFilesService, LocalFilesService],
   exports: [GitFilesService],
 })
 export class GitFilesModule {}
