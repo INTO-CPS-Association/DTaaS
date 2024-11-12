58ff8a6c494651902aee43abe2eec0fd25b305c5
vanessa <vanessascherma@gmail.com>
2024-11-12 20:00:13 +0100

    Working integration tests

--- client/test/integration/jest.setup.ts
+++ client/test/integration/jest.setup.ts
@@ -5,3 +5,9 @@ import 'test/__mocks__/global_mocks';
 beforeEach(() => {
   jest.resetAllMocks();
 });
+
+global.window.env = {
+  ...global.window.env,
+  REACT_APP_AUTH_AUTHORITY: process.env.REACT_APP_AUTH_AUTHORITY || 'https://example.com',
+};
+
