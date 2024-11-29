83587f782c8dea5bda4435d89f734ed2e936c106
atomicgamedev <enokmalik@gmail.com>
2024-11-29 23:55:40 +0100

    Integrate verification and sign in

--- client/src/route/auth/Signin.tsx
+++ client/src/route/auth/Signin.tsx
@@ -2,16 +2,69 @@ import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-
 import { useAuth } from 'react-oidc-context';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
+import { CircularProgress } from '@mui/material';
+import VerifyConfig, {
+  getValidationResults,
+  validationType,
+} from './VerifyConfig';
 
 function SignIn() {
   const auth = useAuth();
+  const [validationResults, setValidationResults] = useState<{
+    [key: string]: validationType;
+  }>({});
+  const [isLoading, setIsLoading] = useState(true);
+  const configsToVerify = [
+    'url',
+    'auth_authority',
+    'redirect_uri',
+    'logout_redirect_uri',
+  ];
+  const verifyConfig = VerifyConfig({
+    keys: configsToVerify,
+    title: 'Config validation failed',
+  });
+
+  React.useEffect(() => {
+    const fetchValidationResults = async () => {
+      const results = await getValidationResults();
+      setValidationResults(results);
+      setIsLoading(false);
+    };
+    fetchValidationResults();
+  }, []);
 
   const startAuthProcess = () => {
     auth.signinRedirect();
   };
+
+  if (isLoading) {
+    return (
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        Verifying configuration
+        <CircularProgress />
+      </Box>
+    );
+  }
+  if (
+    configsToVerify.reduce(
+      (accumulator, currentValue) =>
+        accumulator || validationResults[currentValue].error !== undefined,
+      false,
+    )
+  ) {
+    return verifyConfig;
+  }
 
   return (
     <Box
