e533f21cd37ff357b76426e06c1d0f21b38e839e
atomicgamedev <enokmalik@gmail.com>
2025-01-01 21:34:12 +0100

    Refactor Signin function

--- client/src/route/auth/Signin.tsx
+++ client/src/route/auth/Signin.tsx
@@ -13,6 +13,15 @@ function SignIn() {
   };
 
   return (
+    <BoxForSignIn>
+      {avatar}
+      {signInButton(startAuthProcess)}
+    </BoxForSignIn>
+  );
+}
+
+function BoxForSignIn(props: { children: React.ReactNode }) {
+  return (
     <Box
       sx={{
         marginTop: 8,
@@ -21,46 +30,55 @@ function SignIn() {
         alignItems: 'center',
       }}
     >
-      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-        <LockOutlinedIcon />
-      </Avatar>
-      <Button
-        onClick={startAuthProcess}
-        variant="contained"
-        sx={{
-          display: 'inline-flex',
-          alignItems: 'center',
-          padding: '10px 20px',
-          backgroundColor: '#fc6d27',
-          color: 'white',
-          border: 'none',
-          borderRadius: '5px',
-          fontSize: '16px',
-          fontWeight: 'bold',
-          textDecoration: 'none',
-          textTransform: 'none',
-          '&:hover': {
-            backgroundColor: '#fc6d27',
-            textDecoration: 'none',
-          },
-        }}
-        startIcon={
-          <img
-            src={
-              'https://gitlab.com/gitlab-com/gitlab-artwork/-/raw/master/logo/logo-square.png'
-            }
-            alt="GitLab logo"
-            style={{
-              height: '20px',
-              marginRight: '10px',
-            }}
-          />
-        }
-      >
-        Sign In with GitLab
-      </Button>
+      {props.children}
     </Box>
   );
 }
+
+const avatar: JSX.Element = (
+  <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+    <LockOutlinedIcon />
+  </Avatar>
+);
+
+const signInButton = (startAuthProcess: () => void) => (
+  <Button
+    onClick={startAuthProcess}
+    variant="contained"
+    sx={{
+      display: 'inline-flex',
+      alignItems: 'center',
+      padding: '10px 20px',
+      backgroundColor: '#fc6d27',
+      color: 'white',
+      border: 'none',
+      borderRadius: '5px',
+      fontSize: '16px',
+      fontWeight: 'bold',
+      textDecoration: 'none',
+      textTransform: 'none',
+      '&:hover': {
+        backgroundColor: '#fc6d27',
+        textDecoration: 'none',
+      },
+    }}
+    startIcon={startIcon}
+  >
+    Sign In with GitLab
+  </Button>
+);
+
+const startIcon = (
+  <img
+    src={
+      'https://gitlab.com/gitlab-com/gitlab-artwork/-/raw/master/logo/logo-square.png'
+    }
+    alt="GitLab logo"
+    style={{
+      height: '20px',
+      marginRight: '10px',
+    }}
+  />
+);
 
 export default SignIn;
