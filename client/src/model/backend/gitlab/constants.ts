1506b2f30dc141a9f0ce65de382b91753926326a
atomic <enokmalik@gmail.com>
2025-07-02 19:27:04 +0200

    Add some documentation, minor changes

--- client/src/model/backend/gitlab/constants.ts
+++ client/src/model/backend/gitlab/constants.ts
@@ -23,6 +23,7 @@ export const defaultFiles = [
   { name: 'README.md', type: 'description' },
   { name: '.gitlab-ci.yml', type: 'config' },
 ];
+
 export enum FileType {
   DESCRIPTION = 'description',
   CONFIGURATION = 'configuration',
