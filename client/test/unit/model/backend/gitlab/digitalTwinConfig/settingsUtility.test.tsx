ba4ef7a49ad88adf5bd79edfb809b30eeccfe1b8
atomic <enokmalik@gmail.com>
2026-08-16 18:39:25 +0200

    Delete duplicated tests from client

--- client/test/unit/model/backend/gitlab/digitalTwinConfig/settingsUtility.test.tsx
+++ client/test/unit/model/backend/gitlab/digitalTwinConfig/settingsUtility.test.tsx
@@ -1,32 +1,14 @@
 import {
-  getGroupName,
-  getCommonLibraryProjectName,
-  getDTDirectory,
-  getRunnerTag,
-  getBranchName,
-  getLoggingEnabled,
-  resetSettingsStore,
-} from 'model/backend/gitlab/digitalTwinConfig/settingsUtility';
-import {
   useGroupName,
   useCommonLibraryProjectName,
   useDTDirectory,
   useRunnerTag,
   useBranchName,
 } from 'util/settingsUseHooks';
-import store from 'store/store';
-import {
-  setGroupName,
-  setCommonLibraryProjectName,
-  setDTDirectory,
-  setRunnerTag,
-  DEFAULT_SETTINGS,
-  setBranchName,
-} from 'store/settings.slice';
+import { DEFAULT_SETTINGS } from 'store/settings.slice';
 import { renderHook } from '@testing-library/react';
 import { useSelector } from 'react-redux';
 
-jest.unmock('model/backend/gitlab/digitalTwinConfig/settingsUtility');
 jest.mock('react-redux', () => ({
   ...jest.requireActual('react-redux'),
   useSelector: jest.fn(),
@@ -34,7 +16,7 @@ jest.mock('react-redux', () => ({
 
 const mockedUseSelector = useSelector as unknown as jest.Mock;
 
-describe('Constants', () => {
+describe('settingsUseHooks', () => {
   let settings: typeof DEFAULT_SETTINGS;
 
   function updateSettingsMock(
@@ -85,25 +67,5 @@ describe('Constants', () => {
     expect(dtDir).toBe(DEFAULT_SETTINGS.DT_DIRECTORY);
     expect(runnerTag).toBe(DEFAULT_SETTINGS.RUNNER_TAG);
     expect(branchName).toBe(DEFAULT_SETTINGS.BRANCH_NAME);
-  });
-
-  it('return correct values from global store', () => {
-    store.dispatch(setGroupName('testGroup'));
-    store.dispatch(setCommonLibraryProjectName('testCommon'));
-    store.dispatch(setDTDirectory('testDT'));
-    store.dispatch(setRunnerTag('testRunner'));
-    store.dispatch(setBranchName('testBranch'));
-
-    expect(getGroupName()).toBe('testGroup');
-    expect(getCommonLibraryProjectName()).toBe('testCommon');
-    expect(getDTDirectory()).toBe('testDT');
-    expect(getRunnerTag()).toBe('testRunner');
-    expect(getBranchName()).toBe('testBranch');
-  });
-
-  it('returns false for logging when the settings store is not initialized', () => {
-    resetSettingsStore();
-
-    expect(getLoggingEnabled()).toBe(false);
   });
 });
